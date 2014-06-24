5ee078444ceea58789231f975a1e8d064920538b
Urtzi Odriozola <uodriozola@codesyntax.com>
2014-06-24 12:40:17 +0200

    Aldaketa proiektuan

--- tutoreus/base/admin.py
+++
@@ -1,9 +0,0 @@
-from tutoreus.base.models import Base
-from django.contrib import admin
-from tutoreus.base.forms import BaseAdminForm
-    
-class BaseAdmin(admin.ModelAdmin):
-    list_display = ('izenburua','azpi_izenburua')
-    form = BaseAdminForm
-    
-admin.site.register(Base, BaseAdmin)
