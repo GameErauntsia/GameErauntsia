75931b06769ff4fbb4ca45757205200f0a8108f9
urtzai <uodriozola@codesyntax.com>
2014-07-17 19:02:23 +0200

    template bugfix

--- gamerauntsia/gameplaya/urls.py
+++ gamerauntsia/gameplaya/urls.py
@@ -3,10 +3,10 @@ from gamerauntsia import settings
 
 urlpatterns = patterns('',
     url(r'^$', 'gamerauntsia.gameplaya.views.index', name='gameplay_index'),
-    url(r'^kategoriak/(?P<gaia>[-\w]+)/$', 'gamerauntsia.gameplaya.views.index', name='gameplay_gaia'),
-    url(r'^jokalariak/(?P<username>[-\w]+)/$', 'gamerauntsia.gameplaya.views.index', name='gameplay_user'),
-    url(r'^mailak/(?P<maila>[-\w]+)/$', 'gamerauntsia.gameplaya.views.index', name='gameplay_level'),
-    url(r'^(?P<slug>[-\w]+)/$', 'gamerauntsia.gameplaya.views.gameplaya', name='gameplay'),
+    url(r'^kategoriak/(?P<gaia>[-\w]+)$', 'gamerauntsia.gameplaya.views.index', name='gameplay_gaia'),
+    url(r'^jokalariak/(?P<username>[-\w]+)$', 'gamerauntsia.gameplaya.views.index', name='gameplay_user'),
+    url(r'^mailak/(?P<maila>[-\w]+)$', 'gamerauntsia.gameplaya.views.index', name='gameplay_level'),
+    url(r'^(?P<slug>[-\w]+)$', 'gamerauntsia.gameplaya.views.gameplaya', name='gameplay'),
 )
 
 if settings.DEBUG:
