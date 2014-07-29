2e2aa5c381153f2da1abd22c357917ce671d560a
urtzai <uodriozola@codesyntax.com>
2014-07-29 13:14:44 +0200

    Profile bugfix

--- gamerauntsia/gamer/views.py
+++ gamerauntsia/gamer/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 from django.shortcuts import get_object_or_404
 
 def index(request):
-    users = User.object.all()
+    users = User.objects.all()
     return render_to_response('gamer/index.html', locals(),context_instance=RequestContext(request))
     
 def profile(request,username):
