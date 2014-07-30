55f9c767fb49dc34856e485ec700d17ca6568e37
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2014-07-31 01:02:36 +0200

    Game admin inproved

--- gamerauntsia/jokoa/admin.py
+++ gamerauntsia/jokoa/admin.py
@@ -5,7 +5,14 @@ from datetime import datetime
     
 class JokoaAdmin(admin.ModelAdmin):
     list_display = ('izena','bertsioa','url','slug')
-    prepopulated_fields = {"slug": ("izena",)}
+    prepopulated_fields = {"slug": ("izena","bertsioa")}
+
+    fieldsets = (
+
+        ('Datu orokorrak',
+        {'fields':('izena','bertsioa','slug', 'desk')},),
+        ('Osagarriak',
+        {'fields':('lizentzia','url', 'logoa', 'wiki')},),
    
 
 admin.site.register(Jokoa, JokoaAdmin)
