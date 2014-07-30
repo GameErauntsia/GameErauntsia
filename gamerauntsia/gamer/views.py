971299066f28ff1727553a3dd8500782da872fe0
urtzai <uodriozola@codesyntax.com>
2014-07-30 15:24:40 +0200

    Profile template std

--- gamerauntsia/gamer/views.py
+++ gamerauntsia/gamer/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404
 from django.conf import settings
+from django.db.models import Count
 
 def index(request):
     users = GamerUser.objects.all()
@@ -14,4 +15,5 @@ def profile(request,username):
     gameplayak = GamePlaya.objects.filter(publikoa_da=True,erabiltzailea=user).order_by('-pub_date')
     gp_count = len(gameplayak)
     gameplayak = gameplayak[:5]
+    categ = GamePlaya.objects.filter(publikoa_da=True,erabiltzailea=user).values('gaia',).annotate(tw=Count('id'))
     return render_to_response('gamer/profile.html', locals(),context_instance=RequestContext(request))
