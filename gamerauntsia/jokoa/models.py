20f3fae1b6d508b785a014d912f724cb4602a92d
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2014-07-31 00:05:11 +0200

    Game version optional

--- gamerauntsia/jokoa/models.py
+++ gamerauntsia/jokoa/models.py
@@ -9,14 +9,14 @@ SOFTWARE_AUKERAK = (
 
 class Jokoa(models.Model):
     izena = models.CharField(max_length=64)
-    slug = models.SlugField(db_index=True, help_text="Eremu honetan gai honen URL helbidea zehazten ari zara.")
-    desk = models.TextField(max_length=256)
-    bertsioa = models.CharField(max_length=64)
+    slug = models.SlugField(db_index=True, help_text="Eremu honetan joko honen URL helbidea zehazten ari zara.")
+    desk = models.TextField(max_length=256,null=True,blank=True)
+    bertsioa = models.CharField(max_length=64,null=True,blank=True)
     lizentzia = models.CharField(max_length=2, choices=SOFTWARE_AUKERAK)
     
     logoa = models.ForeignKey(Photo,null=True,blank=True)
-    url = models.CharField(max_length=64, help_text="Eremu honetan aplikazio honen atariko URL helbidea zehazten ari zara." )
-    wiki = models.CharField(max_length=64, null=True, blank=True, help_text="Eremu honetan aplikazio honen wikipediako URL helbidea zehaztu mesedez." )
+    url = models.CharField(max_length=64, help_text="Eremu honetan joko honen atariko URL helbidea zehazten ari zara." )
+    wiki = models.CharField(max_length=64, null=True, blank=True, help_text="Eremu honetan joko honen wikipediako URL helbidea zehaztu mesedez." )
     
     
     class Meta:
