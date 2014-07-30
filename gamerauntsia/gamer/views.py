334f1edb5a3142bb06104aa22142a850f0dd35b9
urtzai <uodriozola@codesyntax.com>
2014-07-30 14:24:05 +0200

    Profile template

--- gamerauntsia/gamer/views.py
+++ gamerauntsia/gamer/views.py
@@ -13,4 +13,5 @@ def profile(request,username):
     user = get_object_or_404(GamerUser,username=username)
     gameplayak = GamePlaya.objects.filter(publikoa_da=True,erabiltzailea=user).order_by('-pub_date')
     gp_count = len(gameplayak)
+    gameplayak = gameplayak[:5]
     return render_to_response('gamer/profile.html', locals(),context_instance=RequestContext(request))
