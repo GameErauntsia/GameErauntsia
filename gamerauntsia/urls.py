87b10b1181b12c185f54de3d8abaea050dc6af1e
Urtzi Odriozola <uodriozola@codesyntax.com>
2014-09-04 09:21:04 +0200

    [BUGFIX] HttpResponse import added

--- gamerauntsia/urls.py
+++ gamerauntsia/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from gamerauntsia import settings
 from django.contrib import admin
+from django.http import HttpResponse
 from gamerauntsia.base.feed import LatestEntriesFeed
 
 admin.autodiscover()
