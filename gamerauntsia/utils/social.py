742625c3c8ae1c171f0863cd7903d33caa8aae6b
urtzai <uodriozola@codesyntax.com>
2014-09-10 18:34:52 +0200

    Twitter post bugfix

--- gamerauntsia/utils/social.py
+++ gamerauntsia/utils/social.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 import tweepy
 
-def post_to_twitter(item):
-    if item.publikoa_da:
-        textua = item.getTwitText()
+def post_to_twitter(sender,instance,**kwargs):
+    if instance.publikoa_da:
+        textua = instance.getTwitText()
         auth = tweepy.OAuthHandler(settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET)
         auth.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET)
         api = tweepy.API(auth)  
