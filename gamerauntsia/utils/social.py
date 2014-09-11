e5d16366d355ce7ebf81697ec618836afd4d2e1c
Urtzi Odriozola <uodriozola@codesyntax.com>
2014-09-11 14:17:08 +0200

    twett only when item created

--- gamerauntsia/utils/social.py
+++ gamerauntsia/utils/social.py
@@ -2,7 +2,7 @@ from django.conf import settings
 import tweepy
 
 def post_to_twitter(sender,instance,**kwargs):
-    if instance.publikoa_da:
+    if instance.publikoa_da and created:
         textua = instance.getTwitText()
         auth = tweepy.OAuthHandler(settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET)
         auth.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET)
