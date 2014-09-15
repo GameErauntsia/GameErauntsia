451f0017fe8f4680e2d664869e2d3e4e50589a51
Urtzi Odriozola <uodriozola@codesyntax.com>
2014-09-16 00:54:45 +0200

    news feed added

--- gamerauntsia/base/feed.py
+++ gamerauntsia/base/feed.py
@@ -1,5 +1,6 @@
 from django.contrib.syndication.views import Feed
 from gamerauntsia.gameplaya.models import GamePlaya
+from gamerauntsia.berriak.models import Berria
 
 class LatestEntriesFeed(Feed):
     title = "Gamerauntsia harpidetza"
@@ -14,5 +15,19 @@ class LatestEntriesFeed(Feed):
 
     def item_description(self, item):
         return item.desk
+        
+class LatestNewsFeed(Feed):
+    title = "Gamerauntsia berrien harpidetza"
+    link = "/berriak/"
+    description = "Gamerauntsian argitaratzen diren berriak"
+
+    def items(self):
+        return Berria.objects.order_by('-pub_date')[:20]
+
+    def item_title(self, item):
+        return item.izenburua
+
+    def item_description(self, item):
+        return item.get_desk_txikia()
         
 
