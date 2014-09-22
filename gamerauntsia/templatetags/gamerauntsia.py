237b6efe4ec9655d6883aeb3db15304fd45f3bed
urtzai <uodriozola@codesyntax.com>
2014-09-22 11:16:57 +0200

    Registration form

--- gamerauntsia/templatetags/gamerauntsia.py
+++ gamerauntsia/templatetags/gamerauntsia.py
@@ -1,9 +1,9 @@
 from django import template
-
-register = template.Library()
-
+from gamerauntsia.gamer.models import GamerUser as User
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+
+register = template.Library()
 
 
 class GamerRegistration(forms.Form):
@@ -12,6 +12,22 @@ class GamerRegistration(forms.Form):
                                 label=_("Username"),
                                 error_messages={'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
     email = forms.EmailField(label=_("E-mail"))
+    password1 = forms.CharField(widget=forms.PasswordInput,
+                                label=_("Password"))
+    password2 = forms.CharField(widget=forms.PasswordInput,
+                                label=_("Password (again)"))
+
+    def clean_username(self):
+        """
+        Validate that the username is alphanumeric and is not already
+        in use.
+        
+        """
+        existing = User.objects.filter(username__iexact=self.cleaned_data['username'])
+        if existing.exists():
+            raise forms.ValidationError(_("A user with that username already exists."))
+        else:
+            return self.cleaned_data['username']
 
 def regform(request):
     forma=GamerRegistration()
