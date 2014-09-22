363e220f0b6ca3f25fdce72f4f560ca6b1953981
urtzai <uodriozola@codesyntax.com>
2014-09-22 10:48:52 +0200

    Registration form

--- gamerauntsia/templatetags/gamerauntsia.py
+++ gamerauntsia/templatetags/gamerauntsia.py
@@ -1,10 +1,10 @@
 from django import template
-from gamerauntsia.gamer.forms import MyRegistrationForm
+from registration.forms import RegistrationForm
 
 register = template.Library()
 
 def regform(request):
-    forma=MyRegistrationForm()
+    forma=RegistrationForm()
     return forma.as_p()
 
 register.simple_tag(regform)
