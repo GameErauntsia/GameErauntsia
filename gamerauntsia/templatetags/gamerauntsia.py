8a0ee3dde5f049bbabd7bca4ece73f9d546795e8
urtzai <uodriozola@codesyntax.com>
2014-09-22 11:29:26 +0200

    Django registration

--- gamerauntsia/templatetags/gamerauntsia.py
+++ gamerauntsia/templatetags/gamerauntsia.py
@@ -1,36 +1,12 @@
 from django import template
-# from gamerauntsia.gamer.models import GamerUser as User
+from registration.forms import RegistrationForm
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
-
-class GamerRegistration(forms.Form):
-    username = forms.RegexField(regex=r'^[\w.@+-]+$',
-                                max_length=30,
-                                label=_("Username"),
-                                error_messages={'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
-    email = forms.EmailField(label=_("E-mail"))
-    password1 = forms.CharField(widget=forms.PasswordInput,
-                                label=_("Password"))
-    password2 = forms.CharField(widget=forms.PasswordInput,
-                                label=_("Password (again)"))
-
-    # def clean_username(self):
-    #     """
-    #     Validate that the username is alphanumeric and is not already
-    #     in use.
-        
-    #     """
-    #     existing = User.objects.filter(username__iexact=self.cleaned_data['username'])
-    #     if existing.exists():
-    #         raise forms.ValidationError(_("A user with that username already exists."))
-    #     else:
-    #         return self.cleaned_data['username']
-
 def regform(request):
-    forma=GamerRegistration()
+    forma=RegistrationForm()
     return forma.as_p()
 
 register.simple_tag(regform)
