935e272d8fc3399bc7194fae41f8ecc12e1f954f
urtzai <uodriozola@codesyntax.com>
2014-09-22 10:58:46 +0200

    Registration form

--- gamerauntsia/registration/forms.py
+++ gamerauntsia/registration/forms.py
@@ -28,94 +28,95 @@ class MyRegistrationForm(forms.Form):
     registration backend.
 
     """
-    required_css_class = 'required'
+    pass
+    # required_css_class = 'required'
     
-    username = forms.RegexField(regex=r'^[\w.@+-]+$',
-                                max_length=30,
-                                label=_("Username"),
-                                error_messages={'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
-    email = forms.EmailField(label=_("E-mail"))
-    password1 = forms.CharField(widget=forms.PasswordInput,
-                                label=_("Password"))
-    password2 = forms.CharField(widget=forms.PasswordInput,
-                                label=_("Password (again)"))
+    # username = forms.RegexField(regex=r'^[\w.@+-]+$',
+    #                             max_length=30,
+    #                             label=_("Username"),
+    #                             error_messages={'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
+    # email = forms.EmailField(label=_("E-mail"))
+    # password1 = forms.CharField(widget=forms.PasswordInput,
+    #                             label=_("Password"))
+    # password2 = forms.CharField(widget=forms.PasswordInput,
+    #                             label=_("Password (again)"))
     
-    def clean_username(self):
-        """
-        Validate that the username is alphanumeric and is not already
-        in use.
+    # def clean_username(self):
+    #     """
+    #     Validate that the username is alphanumeric and is not already
+    #     in use.
         
-        """
-        existing = GamerUser.objects.filter(username__iexact=self.cleaned_data['username'])
-        if existing.exists():
-            raise forms.ValidationError(_("A user with that username already exists."))
-        else:
-            return self.cleaned_data['username']
+    #     """
+    #     existing = GamerUser.objects.filter(username__iexact=self.cleaned_data['username'])
+    #     if existing.exists():
+    #         raise forms.ValidationError(_("A user with that username already exists."))
+    #     else:
+    #         return self.cleaned_data['username']
 
-    def clean(self):
-        """
-        Verifiy that the values entered into the two password fields
-        match. Note that an error here will end up in
-        ``non_field_errors()`` because it doesn't apply to a single
-        field.
+    # def clean(self):
+    #     """
+    #     Verifiy that the values entered into the two password fields
+    #     match. Note that an error here will end up in
+    #     ``non_field_errors()`` because it doesn't apply to a single
+    #     field.
         
-        """
-        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
-            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError(_("The two password fields didn't match."))
-        return self.cleaned_data
+    #     """
+    #     if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+    #         if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+    #             raise forms.ValidationError(_("The two password fields didn't match."))
+    #     return self.cleaned_data
 
 
-class MyRegistrationFormTermsOfService(MyRegistrationForm):
-    """
-    Subclass of ``RegistrationForm`` which adds a required checkbox
-    for agreeing to a site's Terms of Service.
+# class MyRegistrationFormTermsOfService(MyRegistrationForm):
+#     """
+#     Subclass of ``RegistrationForm`` which adds a required checkbox
+#     for agreeing to a site's Terms of Service.
     
-    """
-    tos = forms.BooleanField(widget=forms.CheckboxInput,
-                             label=_(u'I have read and agree to the Terms of Service'),
-                             error_messages={'required': _("You must agree to the terms to register")})
+#     """
+#     tos = forms.BooleanField(widget=forms.CheckboxInput,
+#                              label=_(u'I have read and agree to the Terms of Service'),
+#                              error_messages={'required': _("You must agree to the terms to register")})
 
 
-class MyRegistrationFormUniqueEmail(MyRegistrationForm):
-    """
-    Subclass of ``RegistrationForm`` which enforces uniqueness of
-    email addresses.
+# class MyRegistrationFormUniqueEmail(MyRegistrationForm):
+#     """
+#     Subclass of ``RegistrationForm`` which enforces uniqueness of
+#     email addresses.
     
-    """
-    def clean_email(self):
-        """
-        Validate that the supplied email address is unique for the
-        site.
+#     """
+#     def clean_email(self):
+#         """
+#         Validate that the supplied email address is unique for the
+#         site.
         
-        """
-        if GamerUser.objects.filter(email__iexact=self.cleaned_data['email']):
-            raise forms.ValidationError(_("This email address is already in use. Please supply a different email address."))
-        return self.cleaned_data['email']
+#         """
+#         if GamerUser.objects.filter(email__iexact=self.cleaned_data['email']):
+#             raise forms.ValidationError(_("This email address is already in use. Please supply a different email address."))
+#         return self.cleaned_data['email']
 
 
-class MyRegistrationFormNoFreeEmail(MyRegistrationForm):
-    """
-    Subclass of ``RegistrationForm`` which disallows registration with
-    email addresses from popular free webmail services; moderately
-    useful for preventing automated spam registrations.
+# class MyRegistrationFormNoFreeEmail(MyRegistrationForm):
+#     """
+#     Subclass of ``RegistrationForm`` which disallows registration with
+#     email addresses from popular free webmail services; moderately
+#     useful for preventing automated spam registrations.
     
-    To change the list of banned domains, subclass this form and
-    override the attribute ``bad_domains``.
+#     To change the list of banned domains, subclass this form and
+#     override the attribute ``bad_domains``.
     
-    """
-    bad_domains = ['aim.com', 'aol.com', 'email.com', 'gmail.com',
-                   'googlemail.com', 'hotmail.com', 'hushmail.com',
-                   'msn.com', 'mail.ru', 'mailinator.com', 'live.com',
-                   'yahoo.com']
+#     """
+#     bad_domains = ['aim.com', 'aol.com', 'email.com', 'gmail.com',
+#                    'googlemail.com', 'hotmail.com', 'hushmail.com',
+#                    'msn.com', 'mail.ru', 'mailinator.com', 'live.com',
+#                    'yahoo.com']
     
-    def clean_email(self):
-        """
-        Check the supplied email address against a list of known free
-        webmail domains.
+#     def clean_email(self):
+#         """
+#         Check the supplied email address against a list of known free
+#         webmail domains.
         
-        """
-        email_domain = self.cleaned_data['email'].split('@')[1]
-        if email_domain in self.bad_domains:
-            raise forms.ValidationError(_("Registration using free email addresses is prohibited. Please supply a different email address."))
-        return self.cleaned_data['email']
+#         """
+#         email_domain = self.cleaned_data['email'].split('@')[1]
+#         if email_domain in self.bad_domains:
+#             raise forms.ValidationError(_("Registration using free email addresses is prohibited. Please supply a different email address."))
+#         return self.cleaned_data['email']
