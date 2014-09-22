f44546daf8a9494e3b9ab042ab5c2767ecbc521d
urtzai <uodriozola@codesyntax.com>
2014-09-22 11:14:14 +0200

    Registration form

--- gamerauntsia/templatetags/gamerauntsia.py
+++ gamerauntsia/templatetags/gamerauntsia.py
@@ -1,7 +1,17 @@
 from django import template
-from gamerauntsia.registration.forms import GamerRegistration
 
 register = template.Library()
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class GamerRegistration(forms.form):
+    username = forms.RegexField(regex=r'^[\w.@+-]+$',
+                                max_length=30,
+                                label=_("Username"),
+                                error_messages={'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
+    email = forms.EmailField(label=_("E-mail"))
 
 def regform(request):
     forma=GamerRegistration()
