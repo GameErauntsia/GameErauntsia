19fa391997b9b98d81659f88991687ec94ac2498
urtzai <uodriozola@codesyntax.com>
2014-09-23 14:39:40 +0200

    News in frontpage

--- gamerauntsia/views.py
+++ gamerauntsia/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import Http404, HttpResponseRedirect
 from gamerauntsia.gameplaya.models import GamePlaya
+from gamerauntsia.berriak.models import Berria
 
 def index(request):
     gameplayak = GamePlaya.objects.filter(publikoa_da=True).order_by('-pub_date')[:4]
+    berriak = Berria.objects.filter(publikoa_da=True).order_by('-pub_date')[:4]
     return render_to_response('index.html', locals(),context_instance=RequestContext(request))
 
 def bilaketa(request,bilatu):
