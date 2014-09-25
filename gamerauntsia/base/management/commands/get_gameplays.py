88fe80720c60f377ba3c9f13e65fd51b63ed1418
Urtzi Odriozola <uodriozola@codesyntax.com>
2014-09-25 22:42:12 +0200

    get gps

--- gamerauntsia/base/management/commands/get_gameplays.py
+++ gamerauntsia/base/management/commands/get_gameplays.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.template import defaultfilters as filters
 from gamerauntsia.utils.urls import get_urljson
 from gamerauntsia.gamer.models import GamerUser
 from gamerauntsia.auto_gameplay.models import AutoGamePlaya
@@ -31,7 +32,7 @@ def get_gameplays():
 
                     auto = AutoGamePlaya()
                     auto.izenburua = video['title']['$t']
-                    auto.slug = slugify(video['title']['$t'])
+                    auto.slug = filters.slugify(video['title']['$t'])
                     auto.desk = video['media$group']['media$description']['$t']
                     duration = datetime.timedelta(seconds=int(video['media$group']['yt$duration']['seconds']))
                     auto.iraupena_min = duration.minute
@@ -39,16 +40,13 @@ def get_gameplays():
 
                     photo = Photo()
                     name = urlparse(img_url).path.split('/')[-1]
-                    photo.image.save(name, File(urllib2.urlopen(self.url).read(), save=True)
+                    photo.image.save(name, File(urllib2.urlopen(self.url).read(), save=True))
 
                     auto.argazkia = photo
                     auto.bideoa = video['media$group']['yt$videoid']['$t']
                     auto.erabiltzailea = gamer
                     auto.save()
-                    
-                    break
-        break
-        
+    return True                
 
 class Command(BaseCommand):
     help = 'Get GPs automaticaly'
