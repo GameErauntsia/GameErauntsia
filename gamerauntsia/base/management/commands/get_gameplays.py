429403d66e6e20452756035fee68b7ad4dd7fc23
urtzai <uodriozola@codesyntax.com>
2014-09-25 17:36:30 +0200

    Auto gameplay system

--- gamerauntsia/base/management/commands/get_gameplays.py
+++ gamerauntsia/base/management/commands/get_gameplays.py
@@ -1,12 +1,18 @@
 from django.core.management.base import BaseCommand
 from gamerauntsia.utils.urls import get_urljson
 from gamerauntsia.gamer.models import GamerUser
+from gamerauntsia.auto_gameplay.models import AutoGamePlaya
  
 def get_gameplays():
     gamers = GamerUser.objects.filter(is_gamer=True,ytube_channel__isnull=False)
 
     for gamer in gamers:
         channel = 'http://gdata.youtube.com/feeds/api/users/'+gamer.username+'/uploads?v=2&alt=json'
+        data = get_urljson(channel)
+
+        print data
+
+        break
 
 class Command(BaseCommand):
     help = 'Get GPs automaticaly'
