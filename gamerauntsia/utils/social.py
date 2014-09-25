f68729b8fcf800471348b2d0dde68e03a751e2d1
urtzai <uodriozola@codesyntax.com>
2014-09-25 09:50:47 +0200

    Social bugfix

--- gamerauntsia/utils/social.py
+++ gamerauntsia/utils/social.py
@@ -62,9 +62,10 @@ def send_to_fb_page(item, attachment):
         return 0
     try:
         page_graph = facebook.GraphAPI(page_access_token)
-        page_graph.put_object(PAGE_ID, FB_PAGE, attachment)
+        page_graph.put_object(PAGE_ID, attachment)
     except Exception, e:
         logging.error('Errorea FBra bidaltzean: %(error)s' % {'error': e})
+        logging.error(attachment)
     return 1
     
 
