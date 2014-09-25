08dd51807592e1bc22caa83e5b9109f817f5d522
urtzai <uodriozola@codesyntax.com>
2014-09-25 15:19:32 +0200

    Mail sending

--- gamerauntsia/base/views.py
+++ gamerauntsia/base/views.py
@@ -8,14 +8,11 @@ MESSAGE = 'Mezu berri bat utzi dute zuk idatzitako gai honetan: '
 def send_email(sender,instance,**kwargs):
     if kwargs['created']:
         recipient_list = []
-        message = MESSAGE + '%s%s%s' % (settings.HOST,instance.topic.forum.slug,instance.topic.id)
-        creators = Post.objects.filter(topic=instance.topic).values('creator').distinct()
+        message = MESSAGE + '%s%s/%s' % (settings.HOST,instance.topic.forum.slug,instance.topic.id)
+        creators = Post.objects.filter(topic=instance.topic).values('creator__email').distinct()
         for creator in creators:
-            recipient_list.append(creator.email)
+            recipient_list.append(creator['creator__email'])
 
-        send_mail('['+instance.topic.title+']', message, settings.DEFAULT_FROM_EMAIL, recipient_list)
+        send_mail('[Game Erauntsia - '+instance.topic.title+']', message, settings.DEFAULT_FROM_EMAIL, recipient_list)
 
 post_save.connect(send_email, sender=Post)
-
-
-
