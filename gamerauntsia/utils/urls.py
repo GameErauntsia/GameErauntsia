a297bbde3b24831f72ff612dad0468f6907c200d
urtzai <uodriozola@codesyntax.com>
2014-09-25 17:02:55 +0200

    GET JSON method

--- gamerauntsia/utils/urls.py
+++ gamerauntsia/utils/urls.py
@@ -1,20 +1,5 @@
-from urlparse2 import urlparse
 import urllib2
 import json
-
-def get_parsed_url(url):
-    """ """
-    if url:
-        return urlparse(url)
-    else:
-        return url
-    
-def get_domain(url):
-
-    hostname = get_parsed_url(url).hostname
-    if hostname and hostname.startswith('www.'):
-        hostname = hostname[4:]
-    return hostname
 
 def get_urljson(url):
     stream = []
