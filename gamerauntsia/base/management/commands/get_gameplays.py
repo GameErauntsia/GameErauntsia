0c4f5b5d4540eba72fd0024690ac5727191e4182
urtzai <uodriozola@codesyntax.com>
2014-09-26 12:38:47 +0200

    Auto gameplay system completed

--- gamerauntsia/base/management/commands/get_gameplays.py
+++ gamerauntsia/base/management/commands/get_gameplays.py
@@ -4,6 +4,7 @@ from gamerauntsia.utils.urls import get_urljson
 from gamerauntsia.gamer.models import GamerUser
 from gamerauntsia.auto_gameplay.models import AutoGamePlaya
 from gamerauntsia.gameplaya.models import GamePlaya
+from gamerauntsia.utils.image import loadUrlImage
 from photologue.models import Photo
 import time
 import urllib2
