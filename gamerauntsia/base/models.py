74c4b846fc12d556951296008248e8d60e2df2a7
urtzai <uodriozola@codesyntax.com>
2014-09-30 19:17:56 +0200

    Forum messages for multiple forum

--- gamerauntsia/base/models.py
+++ gamerauntsia/base/models.py
@@ -3,12 +3,14 @@ from django.core.mail import send_mail
 from django_simple_forum.models import Post
 from django.db.models.signals import post_save
 
-MESSAGE = 'Mezu berri bat utzi dute zuk idatzitako gai honetan: '
+MESSAGE = 'Mezu berri bat utzi dute zuk idatzitako gai hauetan: \n\n'
 
 def send_email(sender,instance,**kwargs):
     if kwargs['created']:
         recipient_list = []
-        message = MESSAGE + '%sforoa/%s/%s' % (settings.HOST,instance.topic.forum.slug,instance.topic.id)
+        message = MESSAGE
+        for forum in instance.topic.forum.all():
+        	message += '%sforoa/%s/%s\n\n' % (settings.HOST,forum.slug,instance.topic.id)
         creators = Post.objects.filter(topic=instance.topic).values('creator__email').distinct()
         for creator in creators:
             if not instance.creator.email == creator['creator__email'] and instance.creator.email_notification:
