7af393abad946422d7a56a5cca14b156a9c6b4f0
urtzai <uodriozola@codesyntax.com>
2014-10-01 19:37:26 +0200

    Gamer platforms

--- gamerauntsia/gamer/views.py
+++ gamerauntsia/gamer/views.py
@@ -62,6 +62,6 @@ def edit_platform(request):
             gameformset.save()
             return HttpResponseRedirect(reverse('edit_profile_plat'))
     else:
-        gameformset = GameFormSet(initial=[{'user': user,},{'user': user,},{'user': user,}])
+        gameformset = GameFormSet()
 
     return render_to_response('profile/edit_platform.html', locals(), context_instance=RequestContext(request))
