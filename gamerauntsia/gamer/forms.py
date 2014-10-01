cf82e45d374ef0bdcaaab087896566f7c47527f1
urtzai <uodriozola@codesyntax.com>
2014-10-01 17:10:32 +0200

    Notifications

--- gamerauntsia/gamer/forms.py
+++ gamerauntsia/gamer/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from hiribili.hiribiliuser.models import GamerUser
+from gamerauntsia.gamer.models import GamerUser
 from django.utils.translation import ugettext as _
 
 class NotifyForm(forms.ModelForm):
