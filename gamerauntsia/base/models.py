3eefad139310e83840f939042efa27e97ce2b965
urtzai <uodriozola@codesyntax.com>
2014-10-01 13:54:49 +0200

    Forum admin notification

--- gamerauntsia/base/models.py
+++ gamerauntsia/base/models.py
@@ -1,14 +1,12 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from django_simple_forum.models import Post
+from django_simple_forum.models import Post, Topic
 from django.db.models.signals import post_save
 
-MESSAGE = 'Mezu berri bat utzi dute zuk idatzitako gai hauetan: \n\n'
-
-def send_email(sender,instance,**kwargs):
+def send_post_email(sender,instance,**kwargs):
     if kwargs['created']:
         recipient_list = []
-        message = MESSAGE
+        message = 'Mezu berri bat utzi dute zuk idatzitako gai hauetan: \n\n'
         for forum in instance.topic.forums.all():
         	message += '%sforoa/%s/%s\n\n' % (settings.HOST,forum.slug,instance.topic.id)
         creators = Post.objects.filter(topic=instance.topic).values('creator__email').distinct()
@@ -16,4 +14,12 @@ def send_email(sender,instance,**kwargs):
             if not instance.creator.email == creator['creator__email'] and instance.creator.email_notification:
                 send_mail('[Game Erauntsia - '+instance.topic.title+']', message, settings.DEFAULT_FROM_EMAIL, [creator['creator__email']])
 
-post_save.connect(send_email, sender=Post)
+def send_topic_email(sender,instance,**kwargs):
+    if kwargs['created']:
+        message = 'Gai berri bat sortu dute: \n\n%sforoa/%s/%s\n\n' % (settings.HOST,instance.forums.all().order_by('-id')[0].slug,instance.id)
+        creator = instance.forums.all().order_by('-id')[0].creator
+        creator.email_user(subject='[Game Erauntsia - '+instance.title+']', message=message, from_email=settings.DEFAULT_FROM_EMAIL)
+
+
+post_save.connect(send_topic_email, sender=Topic)
+post_save.connect(send_post_email, sender=Post)
