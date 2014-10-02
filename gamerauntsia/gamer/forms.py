a457594c64d4852cd8a9b94ef0ae00e435cb870c
urtzai <uodriozola@codesyntax.com>
2014-10-02 11:53:46 +0200

    FormSet bugfix

--- gamerauntsia/gamer/forms.py
+++ gamerauntsia/gamer/forms.py
@@ -9,7 +9,7 @@ class NotifyForm(forms.ModelForm):
         fields = ('email_notification',)
 
 class GameForm(forms.ModelForm):
-    user = forms.CharField(widget=forms.HiddenInput(),required=False)
+    user = forms.IntegerField(widget=forms.HiddenInput(),required=False)
 
     class Meta:
         model = JokuPlataforma
