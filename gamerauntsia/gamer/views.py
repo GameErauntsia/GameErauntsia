89a83938c295f16040ade5091a0a2d460c4f3603
urtzai <uodriozola@codesyntax.com>
2014-10-02 17:57:50 +0200

    Platforms

--- gamerauntsia/gamer/views.py
+++ gamerauntsia/gamer/views.py
@@ -14,7 +14,7 @@ from django.contrib import messages
 from photologue.models import Photo
 from gamerauntsia.gamer.forms import NotifyForm,GameForm
 from django.utils.translation import ugettext as _
-from django.forms.models import inlineformset_factory
+from django.forms.models import modelformset_factory
 
 def index(request):
     users = GamerUser.objects.filter(is_active=True,is_staff=True).order_by('-date_joined')
@@ -54,16 +54,18 @@ def edit_platform(request):
     """ """
     tab = 'platforms'
     user = request.user
-    GameFormSet = inlineformset_factory(JokuPlataforma, form=GameForm, can_delete=True, max_num=1)
+    GameFormSet = modelformset_factory(JokuPlataforma, form=GameForm, can_delete=True)
     if request.method == 'POST':
          posta=request.POST.copy()     
          gameformset = GameFormSet(posta)
          if gameformset.is_valid():
-            for form in gameformset:
-                if form.is_valid():
-                    platform = form.save(commit=False)
-                    platform.user = user
-                    platform.save()
+            marked_for_delete = formset.deleted_forms
+            if form['id'].value() not in [deleted_record['id'].value() for deleted_record in marked_for_delete]:
+                for form in gameformset:
+                    if form.is_valid():
+                        platform = form.save(commit=False)
+                        platform.user = user
+                        platform.save()
             return HttpResponseRedirect(reverse('edit_profile_plat'))
     else:
         qset = JokuPlataforma.objects.filter(user=user)
