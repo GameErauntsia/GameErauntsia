13f305146405ec91e049a19d04c9c9c0450cb374
urtzai <uodriozola@codesyntax.com>
2014-10-02 11:31:37 +0200

    FormSet bugfix

--- gamerauntsia/gamer/views.py
+++ gamerauntsia/gamer/views.py
@@ -61,7 +61,7 @@ def edit_platform(request):
          if gameformset.is_valid():
             for form in gameformset:
                 if form.is_valid():
-                    form.cleaned_data['user_id'] = user
+                    form.cleaned_data['user'] = user
                     form.save()
             return HttpResponseRedirect(reverse('edit_profile_plat'))
     else:
