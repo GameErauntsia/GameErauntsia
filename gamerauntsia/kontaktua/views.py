3088be61cfefcfa0a09178f3751a75c8a41fd6e3
urtzai <uodriozola@codesyntax.com>
2014-10-03 16:47:29 +0200

    Contact bugfix

--- gamerauntsia/kontaktua/views.py
+++ gamerauntsia/kontaktua/views.py
@@ -3,6 +3,7 @@ from gamerauntsia.berriak.models import Berria
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.mail import send_mail
+from django.template import loader
 
 def index(request):
     h = {}
@@ -14,6 +15,7 @@ def bidali(request):
     eposta = request.POST.get('eposta')
     gaia = request.POST.get('gaia')
     iruzkina = request.POST.get('iruzkina')
-    send_mail(settings.EMAIL_SUBJECT+' '+gaia, iruzkina, eposta,[settings.DEFAULT_TO_EMAIL], fail_silently=False)
+    tmpl = loader.render_to_string('kontaktua/email_tmpl.html',{'email':eposta,'body': iruzkina})
+    send_mail(settings.EMAIL_SUBJECT+' '+gaia, tmpl, settings.DEFAULT_FROM_EMAIL,[settings.DEFAULT_TO_EMAIL], fail_silently=False)
     h['berriak'] = Berria.objects.all().order_by('-pub_date')[:5]
     return render_to_response('kontaktua/bidalita.html', h,context_instance=RequestContext(request))
