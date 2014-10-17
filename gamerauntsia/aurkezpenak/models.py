71bf31ae6c78d3f9e619e1a4f5e67104d344838b
urtzai <uodriozola@codesyntax.com>
2014-10-17 17:15:25 +0200

    Presentations

--- gamerauntsia/aurkezpenak/models.py
+++ gamerauntsia/aurkezpenak/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.conf import settings
 from datetime import datetime
+from gamerauntsia.gamer.models import GamerUser
 from django.template import defaultfilters as filters
 
 class Aurkezpena(models.Model):
     izena = models.CharField(max_length=64)
-    slug = models.SlugField(db_index=True, unique=True, help_text="Eremu honetan kategoria honen URL helbidea zehazten ari zara.")
-    template_name =  models.CharField(max_length=64)
+    slug = models.SlugField(db_index=True, unique=True, help_text="Eremu honetan aurkezpen honen URL helbidea zehazten ari zara.")
+    abstract = models.TextField(max_length=256)
+    slides = models.TextField()
+
+    erabiltzailea = models.ForeignKey(GamerUser)
 
     pub_date = models.DateTimeField('publikazio data', default=datetime.now)
     mod_date = models.DateTimeField('modifikazio data', default=datetime.now)
