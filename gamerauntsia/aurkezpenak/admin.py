5386cc07d62741a5da63b424e3a74f0aa1bb1a31
urtzai <uodriozola@codesyntax.com>
2014-10-17 17:17:19 +0200

    Presentations

--- gamerauntsia/aurkezpenak/admin.py
+++ gamerauntsia/aurkezpenak/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from gamerauntsia.aurkezpenak.models import Aurkezpena
 
 class AurkezpenaAdmin(admin.ModelAdmin):
-    list_display = ('izena', 'slug','template_name')
+    list_display = ('izena', 'slug','erabiltzailea')
     prepopulated_fields = {"slug": ("izena",)}
     list_filter = ('izena',)
-    search_fields = ['izena','template_name',]
+    search_fields = ['izena','slug',]
 
 admin.site.register(Aurkezpena, AurkezpenaAdmin)
