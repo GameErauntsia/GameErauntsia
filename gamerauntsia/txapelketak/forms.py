9a9bea530b4f8b26ab0420aef7b09f34aad1a97c
urtzai <uodriozola@codesyntax.com>
2014-11-07 15:47:25 +0100

    Txapelketa improvements

--- gamerauntsia/txapelketak/forms.py
+++ gamerauntsia/txapelketak/forms.py
@@ -4,6 +4,6 @@ from models import Partaidea
 class PartidaInlineForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PartidaInlineForm, self).__init__(*args, **kwargs)
-        if self.instance.txapelketa:
+        if self.instance:
         	self.fields['partaideak'].queryset = Partaidea.objects.filter(
             txapelketa=self.instance.txapelketa)
