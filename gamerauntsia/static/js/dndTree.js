fb25c9145063d4cd2c442954ad83c66170de8d7a
urtzai <uodriozola@codesyntax.com>
2014-11-11 17:27:53 +0100

    Tournament graph

--- gamerauntsia/static/js/dndTree.js
+++ gamerauntsia/static/js/dndTree.js
@@ -1,5 +1,5 @@
 // Get JSON data
-treeJSON = d3.json("flare.json", function(error, treeData) {
+treeJSON = d3.json("/static/js/flare.json", function(error, treeData) {
 
     // Calculate total nodes, max label length
     var totalNodes = 0;
