0435a2526c1a708dd1dc358dab0d3b28940d8a4d
urtzai <uodriozola@codesyntax.com>
2014-12-02 10:23:19 +0100

    Txapelketa datuak

--- gamerauntsia/txapelketak/views.py
+++ gamerauntsia/txapelketak/views.py
@@ -14,32 +14,32 @@ def txapelketa(request,slug):
     list_sailkapena = item.get_partaideak('points')
 
     kanporaketadatuak = [
-                [
-                  [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1},            {"name" : u"Joxepo", "id" : u"Joxepo", "seed" : 2} ],
-                  [ {"name" : u"Garikoitz Larrañaga", "id" : "gari_infernu", "seed" : 3},  {"name" : u"Mitxelon", "id" : u"Mitxelon", "seed" : 4}],
-                  [ {"name" : u"Joseba Galarraga", "id" : "joxhell", "seed" : 5},          {"name" : u"Haritz Odriozola", "id" : "theprodigyeh", "seed" : 6} ],
-                  [ {"name" : u"Eñaut Odriozola", "id" : "eodri", "seed" : 7},             {"name" : u"Iker Bellido", "id" : "robouteguiliam", "seed" : 8}],
-                  [ {"name" : u"Patxi Ordozgoiti", "id" : "ipatx", "seed" : 9},             {"name" : u"Ander Intxaurrondo", "id" : "marmoka", "seed" : 10} ],
-                  [ {"name" : u"yolocaust", "id" : "Olentzero", "seed" : 1},           {"name" : u"Bingen Galartza Iparragirre", "id" : "Galaipa", "seed" : 12}],
-                  [ {"name" : u"Arkaitz Lasarte", "id" : "arklasarte", "seed" : 13},          {"name" : u"Gari Galarza Aizpun", "id" : "Gari", "seed" : 14} ],
-                  [ {"name" : u"Diego Alvarez", "id" : "Gramity", "seed" : 15},          {"name" : u"Iker Ibarguren", "id" : "ikerib", "seed" : 16}]
-                ],
-                [
-                  [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1},            {"name" : u"Garikoitz Larrañaga", "id" : "gari_infernu", "seed" : 3} ],
-                  [ {"name" : u"Joseba Galarraga", "id" : "joxhell", "seed" : 5},          {"name" : u"Iker Bellido", "id" : "robouteguiliam", "seed" : 8} ],
-                  [ {"name" : u"Patxi Ordozgoiti", "id" : "ipatx", "seed" : 10},           {"name" : u"yolocaust", "id" : "Olentzero", "seed" : 12} ],
-                  [ {"name" : u"Gari Galarza Aizpun", "id" : "Gari", "seed" : 14},           {"name" : u"Iker Ibarguren", "id" : "ikerib", "seed" : 15} ]
-                ],
-                [
-                  [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1},            {"name" : u"Joseba Galarraga", "id" : "joxhell", "seed" : 5} ],
-                  [ {"name" : u"Patxi Ordozgoiti", "id" : "ipatx", "seed" : 10},           {"name" : u"Gari Galarza Aizpun", "id" : "Gari", "seed" : 14} ]
-                ],
-                [
-                  [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1},   {"name" : u"Patxi Ordozgoiti", "id" : "ipatx", "seed" : 10} ]
-                ],
-                [
-                  [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1} ]
-                ]
+                # [
+                #   [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1},            {"name" : u"Joxepo", "id" : u"Joxepo", "seed" : 2} ],
+                #   [ {"name" : u"Garikoitz Larrañaga", "id" : "gari_infernu", "seed" : 3},  {"name" : u"Mitxelon", "id" : u"Mitxelon", "seed" : 4}],
+                #   [ {"name" : u"Joseba Galarraga", "id" : "joxhell", "seed" : 5},          {"name" : u"Haritz Odriozola", "id" : "theprodigyeh", "seed" : 6} ],
+                #   [ {"name" : u"Eñaut Odriozola", "id" : "eodri", "seed" : 7},             {"name" : u"Iker Bellido", "id" : "robouteguiliam", "seed" : 8}],
+                #   [ {"name" : u"Patxi Ordozgoiti", "id" : "ipatx", "seed" : 9},             {"name" : u"Ander Intxaurrondo", "id" : "marmoka", "seed" : 10} ],
+                #   [ {"name" : u"yolocaust", "id" : "Olentzero", "seed" : 1},           {"name" : u"Bingen Galartza Iparragirre", "id" : "Galaipa", "seed" : 12}],
+                #   [ {"name" : u"Arkaitz Lasarte", "id" : "arklasarte", "seed" : 13},          {"name" : u"Gari Galarza Aizpun", "id" : "Gari", "seed" : 14} ],
+                #   [ {"name" : u"Diego Alvarez", "id" : "Gramity", "seed" : 15},          {"name" : u"Iker Ibarguren", "id" : "ikerib", "seed" : 16}]
+                # ],
+                # [
+                #   [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1},            {"name" : u"Garikoitz Larrañaga", "id" : "gari_infernu", "seed" : 3} ],
+                #   [ {"name" : u"Joseba Galarraga", "id" : "joxhell", "seed" : 5},          {"name" : u"Iker Bellido", "id" : "robouteguiliam", "seed" : 8} ],
+                #   [ {"name" : u"Patxi Ordozgoiti", "id" : "ipatx", "seed" : 10},           {"name" : u"yolocaust", "id" : "Olentzero", "seed" : 12} ],
+                #   [ {"name" : u"Gari Galarza Aizpun", "id" : "Gari", "seed" : 14},           {"name" : u"Iker Ibarguren", "id" : "ikerib", "seed" : 15} ]
+                # ],
+                # [
+                #   [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1},            {"name" : u"Joseba Galarraga", "id" : "joxhell", "seed" : 5} ],
+                #   [ {"name" : u"Patxi Ordozgoiti", "id" : "ipatx", "seed" : 10},           {"name" : u"Gari Galarza Aizpun", "id" : "Gari", "seed" : 14} ]
+                # ],
+                # [
+                #   [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1},   {"name" : u"Patxi Ordozgoiti", "id" : "ipatx", "seed" : 10} ]
+                # ],
+                # [
+                #   [ {"name" : u"Urtzi Odriozola", "id" : "urtzai", "seed" : 1} ]
+                # ]
             ]
 
     return render_to_response('txapelketak/txapelketa.html', locals(),context_instance=RequestContext(request))
