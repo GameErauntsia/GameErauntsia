f8b9562ffcf40f13e5ac434cf990756da3bb1dfa
urtzai <uodriozola@codesyntax.com>
2014-12-10 16:18:47 +0100

    Tournament winner

--- gamerauntsia/txapelketak/views.py
+++ gamerauntsia/txapelketak/views.py
@@ -38,14 +38,11 @@ def txapelketa(request,slug):
                         graphdata += "],"
                 else:
                     graphdata += "{'name': '???', 'seed': '???', 'id': 0},{'name': '???', 'seed': '???', 'id': 0}]"
-            if x == 0:
-                graphdata += "]"
-            else:
-                graphdata += "],"
+            graphdata += "],"
         try:
-            graphdata += "[{'name','"+item.irabazlea.get_izena()+"','seed':'"+str(item.irabazlea.id)+"','id':"+str(item.irabazlea.id)+"}]]"
+            graphdata += "[[{'name','"+item.irabazlea.get_izena()+"','seed':'"+str(item.irabazlea.id)+"','id':"+str(item.irabazlea.id)+"}]]]"
         except:
-            graphdata += "[{'name','???','seed':'???','id':0}]]"
+            graphdata += "[[{'name','???','seed':'???','id':0}]]]"
 
         #graphdata += "[[[{'name':'Urtzi','seed':1,'id':'urtzai'},{'name':'Jon','seed':2,'id':'jonny'}]],[[{'name':'Urtzi','seed':1,'id':'urtzai'}]]];"            
     else:
