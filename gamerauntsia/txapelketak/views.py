1f9f147d9cf13195adba64f49f5a5e2cde1e2aef
urtzai <uodriozola@codesyntax.com>
2014-12-10 15:50:47 +0100

    Tournament graph

--- gamerauntsia/txapelketak/views.py
+++ gamerauntsia/txapelketak/views.py
@@ -14,8 +14,36 @@ def index(request):
 def txapelketa(request,slug):
     item = get_object_or_404(Txapelketa,slug=slug)
     
-    if item.mota == '0': 
-        kanporaketadatuak = "[[[{'name':'Urtzi','seed':1,'id':'urtzai'},{'name':'Jon','seed':2,'id':'jonny'}]],[[{'name':'Urtzi','seed':1,'id':'urtzai'}]]];"
+    if item.mota == '0':
+        first_node = Partida.objects.get(txapelketa=item,jardunaldia=1)
+        leaflvl = first_node.get_leafnodes(include_self=True)[0].get_level()
+
+        graphdata = "["
+        for x in range(leaflvl,-1,-1):
+            graphdata += "["
+            partidak = Partida.objects.filter(level=x,txapelketa=item)
+            last_part = len(partidak) - 1
+            for j,part in enumerate(partidak):
+                graphdata += "["
+                if part.partaideak.all():
+                    last_gamer = len(part.partaideak.all()) - 1
+                    for i,gamer in enumerate(part.partaideak.all()):
+                        if i == last_gamer:
+                            graphdata += "{'name':"+gamer.get_izena()+", 'seed':"+str(gamer.id)+", 'id':"+str(gamer.id)+"}"
+                        else:
+                            graphdata += "{'name':"+gamer.get_izena()+", 'seed':"+str(gamer.id)+", 'id':"+str(gamer.id)+"},"
+                    if j == last_part:
+                        graphdata += "]"
+                    else:
+                        graphdata += "],"
+                else:
+                    graphdata += "{'name': '???', 'seed': '???', 'id': 0},{'name': '???', 'seed': '???', 'id': 0}]"
+            if x == 0:
+                graphdata += "]]"
+            else:
+                graphdata += "],"
+
+        #graphdata += "[[[{'name':'Urtzi','seed':1,'id':'urtzai'},{'name':'Jon','seed':2,'id':'jonny'}]],[[{'name':'Urtzi','seed':1,'id':'urtzai'}]]];"            
     else:
         list_sailkapena = item.get_partaideak('points')
 
