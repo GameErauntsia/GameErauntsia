dfa44562a14d9072bd84347ced3734c918bc30eb
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2014-12-31 18:23:24 +0100

    MC Server Whitelist

--- gamerauntsia/zerbitzariak/views.py
+++ gamerauntsia/zerbitzariak/views.py
@@ -7,17 +7,17 @@ from gamerauntsia.gamer.models import JokuPlataforma
 
 def minecraft_server(request):
     user = request.user
-    mc_list = MC_Whitelist.objects.all().order_by('-created').value('user')[10]
-    mc_admin_list = MC_Whitelist.objects.filter(rol='a').value('user')
-    mc_vip_list = MC_Whitelist.objects.filter(rol='v').value('user')
+    mc_list = MC_Whitelist.objects.all().order_by('-created').values('user')[10]
+    mc_admin_list = MC_Whitelist.objects.filter(rol='a').values('user')
+    mc_vip_list = MC_Whitelist.objects.filter(rol='v').values('user')
     return render_to_response('zerbitzariak/minecraft.html', locals(),context_instance=RequestContext(request))
 
 def minecraft_add(request):
-	user = request.user
-	if user:
-		if JokuPlataforma.objects.filter(user=user, plataforma='minecraft').exists():
-			ml = MC_Whitelist()
-			ml.mc_user = JokuPlataforma.objects.filter(user=user, plataforma='minecraft')[0].nick
-			ml.user = user
-			ml.save()
-	return HttpResponseRedirect(reverse('minecraft_index'))
+    user = request.user
+    if user:
+        if JokuPlataforma.objects.filter(user=user, plataforma='minecraft').exists():
+            ml = MC_Whitelist()
+            ml.mc_user = JokuPlataforma.objects.filter(user=user, plataforma='minecraft')[0].nick
+            ml.user = user
+            ml.save()
+    return HttpResponseRedirect(reverse('minecraft_index'))
