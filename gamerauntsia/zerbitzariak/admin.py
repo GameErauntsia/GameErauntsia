11a3d38c3de1dea029fadddad7e39a70a56d8b8c
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2014-12-31 15:37:40 +0100

    MC Server Whitelist

--- gamerauntsia/zerbitzariak/admin.py
+++ gamerauntsia/zerbitzariak/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import *
 
 class MC_WhitelistAdmin(admin.ModelAdmin):
-    list_display = ('mc_user','user','rol')
+    list_display = ('mc_user','user','rol','created')
     ordering = ('-created',)
 
 admin.site.register(MC_Whitelist,MC_WhitelistAdmin)
