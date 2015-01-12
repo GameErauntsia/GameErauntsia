d5f26678d18d98c290be18c5654da7c9b6d21ce0
urtzai <uodriozola@codesyntax.com>
2015-01-12 18:12:08 +0100

    Admin editor height

--- gamerauntsia/berriak/forms.py
+++ gamerauntsia/berriak/forms.py
@@ -5,7 +5,7 @@ from tinymce.widgets import TinyMCE
 
 class BerriaAdminForm(forms.ModelForm):
     desk = forms.CharField(widget=TinyMCE(
-               attrs={'cols': 80, 'rows': 30,},mce_attrs=settings.TINYMCE_BODY_CONFIG))
+               attrs={'cols': 80, 'rows': 50,},mce_attrs=settings.TINYMCE_BODY_CONFIG))
 
     class Meta:
         model = Berria
