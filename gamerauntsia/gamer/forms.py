6c066fded40613f66e86858a66432684270d01d3
urtzai <uodriozola@codesyntax.com>
2015-01-29 16:17:21 +0100

    GP form and views bugfix

--- gamerauntsia/gamer/forms.py
+++ gamerauntsia/gamer/forms.py
@@ -84,6 +84,12 @@ class GamePlayForm(forms.ModelForm):
 
     argazkia  = forms.ImageField(label='Nabarmendutako irudia', help_text='Onartutako formatuak: jpg, png, gif.', required=False)
 
+    def clean(self):
+        desk = self.cleaned_data['argazkia']
+        if not argazkia:
+            raise forms.ValidationError('Argazkia jartzea derrigorrezkoa da. Mesedez, jarri argazki polit bat!')
+        return self.cleaned_data['argazkia']
+
     def clean_desk(self):
         """ """
         desk = self.cleaned_data['desk'].strip()
