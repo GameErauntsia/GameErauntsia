eaa90976dec0d3717b899eb25e46a06d4c7656d4
urtzai <uodriozola@codesyntax.com>
2015-02-13 17:48:35 +0100

    Group editors

--- gamerauntsia/gamer/models.py
+++ gamerauntsia/gamer/models.py
@@ -1,6 +1,6 @@
 from cssocialuser.models import CSAbstractSocialUser
 from django.db import models
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import UserManager, Group
 from django.core.mail import send_mail
 from django.conf import settings
 from photologue.models import Photo
