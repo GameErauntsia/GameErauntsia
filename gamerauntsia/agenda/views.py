2bbf93f553e3407ebf9d1da9f1ec0446caa58705
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-02-17 20:51:21 +0100

    Agenda view

--- gamerauntsia/agenda/views.py
+++ gamerauntsia/agenda/views.py
@@ -5,6 +5,6 @@ from django_bootstrap_calendar.models import CalendarEvent
 
 
 def index(request):
-    events = CalendarEvent.objects.filter(start__lt=datetime.now())[:10]
+    events = CalendarEvent.objects.filter(start__gt=datetime.now())[:10]
     return render_to_response('agenda/index.html', locals(),context_instance=RequestContext(request))
 
