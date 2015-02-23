ba6d97613a2cbc6bac9b2766953c388f63938ea6
urtzai <uodriozola@codesyntax.com>
2015-02-23 18:11:49 +0100

    Add event options

--- gamerauntsia/agenda/forms.py
+++ gamerauntsia/agenda/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from django_bootstrap_calendar.models import CalendarEvent
+from django.contrib.admin import widgets
 
 class EventForm(forms.ModelForm):
 
     class Meta:
         model = CalendarEvent
+
+    def __init__(self, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        self.fields['start'].widget = widgets.AdminSplitDateTime()
+        self.fields['end'].widget = widgets.AdminSplitDateTime()
