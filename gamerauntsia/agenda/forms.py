9a663364b2eaeb4db3baf91b41e6f71602b4c193
urtzai <uodriozola@codesyntax.com>
2015-02-24 09:55:36 +0100

    New datetime form

--- gamerauntsia/agenda/forms.py
+++ gamerauntsia/agenda/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from django_bootstrap_calendar.models import CalendarEvent
+from datetimewidget.widgets import DateTimeWidget
 
 class EventForm(forms.ModelForm):
+
+    start_date = forms.DateTimeField(widget=DateTimeWidget(usel10n=True, bootstrap_version=3))
+    end_date = forms.DateTimeField(widget=DateTimeWidget(usel10n=True, bootstrap_version=3))
 
     class Meta:
         model = CalendarEvent
