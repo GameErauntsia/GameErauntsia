090140c0c79dd1067917dcc9ed76415d3faabbf8
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-03-02 13:49:23 +0100

    Update admin.py

--- gamerauntsia/zerbitzariak/admin.py
+++ gamerauntsia/zerbitzariak/admin.py
@@ -3,6 +3,8 @@ from models import *
 
 class MC_WhitelistAdmin(admin.ModelAdmin):
     list_display = ('mc_user','uuid','user','rol','created')
+    search_fields = ['mc_user','user__username',]
+    list_filter = ('rol',)
     ordering = ('-created',)
 
 admin.site.register(MC_Whitelist,MC_WhitelistAdmin)
