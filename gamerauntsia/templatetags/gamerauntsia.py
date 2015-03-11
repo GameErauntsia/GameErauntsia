372a5c5cecfd1b6818a097a4eb966e3aa5702051
urtzai <uodriozola@codesyntax.com>
2015-03-11 18:25:53 +0100

    Forum templates set properly + new templatetag

--- gamerauntsia/templatetags/gamerauntsia.py
+++ gamerauntsia/templatetags/gamerauntsia.py
@@ -29,21 +29,7 @@ def steam_panel():
         return {'steam_group': None}
 register.inclusion_tag('steam_panel.html')(steam_panel)
 
-def callMethod(obj, methodName):
-    method = getattr(obj, methodName)
+@register.filter
+def check_seen(obj,user):
+    return obj.has_seen(user)
 
-    if obj.__dict__.has_key("__callArg"):
-        ret = method(*obj.__callArg)
-        del obj.__callArg
-        return ret
-    return method()
-
-def args(obj, arg):
-    if not obj.__dict__.has_key("__callArg"):
-        obj.__callArg = []
-    
-    obj.__callArg += [arg]
-    return obj
-
-register.filter("call", callMethod)
-register.filter("args", args)
