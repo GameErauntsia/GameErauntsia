72e381f7860f9c6fbe61b00051a87e67b5fd72d9
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-03-13 13:14:45 +0100

    Update admin.py

--- gamerauntsia/base/admin.py
+++ gamerauntsia/base/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 class TerminoaAdmin(admin.ModelAdmin):
     list_display = ('term_eu','term_es','term_en')
+    search_fields = ['term_eu','term_es','term_en']
     ordering = ('term_eu',)
    
 
