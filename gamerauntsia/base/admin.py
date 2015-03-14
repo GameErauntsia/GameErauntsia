4a620f5884470e2b727092bb13332504039e5e5d
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-03-14 16:02:49 +0100

    Update admin.py

--- gamerauntsia/base/admin.py
+++ gamerauntsia/base/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 
 
 class TerminoaAdmin(admin.ModelAdmin):
-    list_display = ('term_eu','term_es','term_en')
+    list_display = ('term_eu','term_es','term_en','jokoa')
     search_fields = ['term_eu','term_es','term_en']
+    raw_id_fields = ('jokoa',)
     ordering = ('term_eu',)
    
 
