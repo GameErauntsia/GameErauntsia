bc86c784d45d934f31ae90e4a4dca55de0df108e
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2015-03-16 23:33:18 +0100

    Typing erro fixed

--- gamerauntsia/jokoa/views.py
+++ gamerauntsia/jokoa/views.py
@@ -6,6 +6,6 @@ from gamerauntsia.jokoa.models import Jokoa
 
 def index(request):
     topjokoak = GamerUser.objects.values('top_jokoak__izena').annotate(Count('top_jokoak')).order_by('-top_jokoak__count','-top_jokoak__izena')[:10]
-    jokoak = Jokoa.objetcts.all()
+    jokoak = Jokoa.objects.all()
     return render_to_response('jokoa/index.html', h,context_instance=RequestContext(request))
    
