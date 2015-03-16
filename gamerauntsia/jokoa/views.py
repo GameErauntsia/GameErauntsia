9e140d9d9e87e7b47cf55ada7bf2559e05f9f56b
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2015-03-16 23:46:22 +0100

    Game list

--- gamerauntsia/jokoa/views.py
+++ gamerauntsia/jokoa/views.py
@@ -6,6 +6,6 @@ from gamerauntsia.jokoa.models import Jokoa
 
 def index(request):
     topjokoak = GamerUser.objects.values('top_jokoak__izena').annotate(Count('top_jokoak')).order_by('-top_jokoak__count','-top_jokoak__izena')[:10]
-    jokoak = Jokoa.objects.all()
+    jokoak = Jokoa.objects.filter(publikoa_da=True).order_by("izena","bertsioa")
     return render_to_response('jokoa/index.html', locals(),context_instance=RequestContext(request))
    
