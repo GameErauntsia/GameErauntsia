5f2561997f03b34ad49331cdcea8b5a79898550c
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2015-03-16 23:34:14 +0100

    locals bugfix

--- gamerauntsia/jokoa/views.py
+++ gamerauntsia/jokoa/views.py
@@ -7,5 +7,5 @@ from gamerauntsia.jokoa.models import Jokoa
 def index(request):
     topjokoak = GamerUser.objects.values('top_jokoak__izena').annotate(Count('top_jokoak')).order_by('-top_jokoak__count','-top_jokoak__izena')[:10]
     jokoak = Jokoa.objects.all()
-    return render_to_response('jokoa/index.html', h,context_instance=RequestContext(request))
+    return render_to_response('jokoa/index.html', locals(),context_instance=RequestContext(request))
    
