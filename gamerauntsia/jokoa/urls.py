5db07a4546f508df168d50390ae7002e18103aed
urtzai <uodriozola@codesyntax.com>
2015-03-17 17:57:09 +0100

    Game page logic

--- gamerauntsia/jokoa/urls.py
+++ gamerauntsia/jokoa/urls.py
@@ -3,10 +3,11 @@ from gamerauntsia import settings
 
 urlpatterns = patterns('',
     url(r'^$', 'gamerauntsia.jokoa.views.index', name='game_index'),
+    url(r'^(?P<slug>[-\w]+)$', 'gamerauntsia.jokoa.views.jokoa', name='game'),
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve',  
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
          {'document_root':     settings.MEDIA_ROOT}),
     )
