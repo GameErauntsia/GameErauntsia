50bfdf037a3bc38283fa14cc272f09c9c6c458ed
urtzai <uodriozola@codesyntax.com>
2015-03-23 17:41:45 +0100

    News and tournaments in games page

--- gamerauntsia/jokoa/views.py
+++ gamerauntsia/jokoa/views.py
@@ -6,6 +6,7 @@ from gamerauntsia.gamer.models import GamerUser
 from gamerauntsia.gameplaya.models import GamePlaya
 from gamerauntsia.base.models import Terminoa
 from gamerauntsia.jokoa.models import Jokoa
+from gamerauntsia.berriak.models import Berria
 from gamerauntsia.utils.urls import get_urljson
 
 def index(request):
@@ -23,4 +24,6 @@ def jokoa(request,slug):
     users = GamerUser.objects.filter(top_jokoak=jokoa,is_staff=False).order_by("-karma")[:6]
     gameplayak = GamePlaya.objects.filter(jokoa=jokoa, publikoa_da=True, status='1')[:2]
     terminoak = Terminoa.objects.filter(jokoa=jokoa).order_by("?")[:10]
+    berriak = Berria.objects.filter(jokoa=jokoa,status='1', pub_date__lt=datetime.now()).order_by('-pub_date')[:3]
+    txapelketak = Txapelketa.objects.filter(jokoa=jokoa,publikoa_da=True).order_by('-pub_date')[:3]
     return render_to_response('jokoa/jokoa.html', locals(),context_instance=RequestContext(request))
