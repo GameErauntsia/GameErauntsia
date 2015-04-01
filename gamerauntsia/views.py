01e085fd98e1a32f7dbac8f7159235a583168fac
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-04-01 16:14:00 +0200

    Update views.py

--- gamerauntsia/views.py
+++ gamerauntsia/views.py
@@ -18,8 +18,13 @@ def index(request):
             txapelketa = list_tx[0]
 
     if Txapelketa.objects.filter(publikoa_da=True,pub_date__lt=datetime.now(),live_bideoa__isnull=False,status='2').exists():
-        tx = Txapelketa.objects.filter(publikoa_da=True,pub_date__lt=datetime.now(),live_bideoa__isnull=False,status='2')[0]
-        match = tx.get_next_match()
+        txs = Txapelketa.objects.filter(publikoa_da=True,pub_date__lt=datetime.now(),live_bideoa__isnull=False,status='2')
+        match = None
+        for tx in txs:
+            if not match:
+                match = tx.get_next_match()
+            elif match>tx.get_next_match()
+                match = tx.get_next_match()
         if match and match<timezone.now():
             live_gp = tx
     return render_to_response('index.html', locals(),context_instance=RequestContext(request))
