f378f8ca0111119fdde06b4d43c0cc22ffcaedb0
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2015-04-17 22:26:56 +0200

    Mumble server page

--- gamerauntsia/zerbitzariak/urls.py
+++ gamerauntsia/zerbitzariak/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from gamerauntsia import settings
 
 urlpatterns = patterns('',
-    url(r'^$', 'gamerauntsia.zerbitzariak.views.minecraft_server', name='minecraft_index'),
-    url(r'^add$', 'gamerauntsia.zerbitzariak.views.minecraft_add', name='minecraft_add'),
+    url(r'^minecraft$', 'gamerauntsia.zerbitzariak.views.minecraft_server', name='minecraft_index'),
+    url(r'^minecraft/add$', 'gamerauntsia.zerbitzariak.views.minecraft_add', name='minecraft_add'),
+    url(r'^mumble$', TemplateView.as_view(template_name='zerbitzariak/mumble.html'), name='mumble_index'),
 )
