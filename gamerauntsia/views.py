052a6817ff7963460080f3e7537af48686bb6884
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-04-27 22:20:38 +0200

    Update views.py

--- gamerauntsia/views.py
+++ gamerauntsia/views.py
@@ -25,8 +25,8 @@ def index(request):
                 match = tx.get_next_match()
             elif match and tx.get_next_match() and match>tx.get_next_match():
                 match = tx.get_next_match()
-        if match and match<timezone.now():
-            live_gp = tx
+        if match and match>timezone.now():
+            live_gp = match.txapelketa
     return render_to_response('index.html', locals(),context_instance=RequestContext(request))
 
 def bilaketa(request,bilatu):
