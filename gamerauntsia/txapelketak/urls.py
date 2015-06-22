dd0d6dec1f014fa080a39006f993fdb2bb925a97
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-06-22 22:55:39 +0200

    Partaide + partida templates

--- gamerauntsia/txapelketak/urls.py
+++ gamerauntsia/txapelketak/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^(?P<slug>[-\w]+)$', 'gamerauntsia.txapelketak.views.txapelketa', name='txapelketa'),
     url(r'^(?P<slug>[-\w]+)/izen_ematea$', 'gamerauntsia.txapelketak.views.txapelketa_insk', name='txapelketa_insk'),
     url(r'^(?P<slug>[-\w]+)/sortu_partaideak$', 'gamerauntsia.txapelketak.views.sortu_partaideak', name='sortu_partaideak'),
-    url(r'^(?P<slug>[-\w]+)/partaidea/(?P<partida>[-\w]+)$', 'gamerauntsia.txapelketak.views.partida', name='partida'),
+    url(r'^(?P<slug>[-\w]+)/partida/(?P<partida>[-\w]+)$', 'gamerauntsia.txapelketak.views.partida', name='partida'),
     url(r'^(?P<slug>[-\w]+)/partaidea/(?P<part_id>[-\w]+)$', 'gamerauntsia.txapelketak.views.partaidea', name='partaidea'),
 )
