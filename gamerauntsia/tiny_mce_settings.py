f600ba10eaca2fd58a60cb269dcc8955509e161f
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-07-02 22:56:07 +0200

    New Tinymce 4.2.1

--- gamerauntsia/tiny_mce_settings.py
+++ gamerauntsia/tiny_mce_settings.py
@@ -4,10 +4,10 @@ TINYMCE_BODY_CONFIG = {
     "plugins": [
             "advlist autolink lists link image charmap print preview anchor",
             "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
+            "insertdatetime media table contextmenu paste",
         ],
-    "toolbar": "styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media | preview"
-    "height" : "300px"
+    "toolbar": "styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media | preview",
+    "height" : "300px",
 }
 
 TINYMCE_SMALL_BODY_CONFIG = {
