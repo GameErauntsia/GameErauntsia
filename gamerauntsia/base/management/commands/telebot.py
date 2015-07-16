1c1e95e6d521a282404e2c4841ff01f4a1d9a9c6
urtzai <uodriozola@codesyntax.com>
2015-07-16 14:52:16 +0200

    Telebot command

--- gamerauntsia/base/management/commands/telebot.py
+++ gamerauntsia/base/management/commands/telebot.py
@@ -4,24 +4,31 @@ import time
 TOKEN = '107547414:AAEXaH2tSNcnaehNq_7NNbNKb1VfDbaa6Qs'
 
 
-def listener(messages):
-    """
-    When new messages arrive TeleBot will call this function.
-    """
-    for m in messages:
-        chatid = m.chat.id
-        if m.content_type == 'text':
-            if 'Kaixo' in m.text:
-            	text = 'Kaixo %s!' % (m.chat.firts_name)
-                tb.send_message(chatid, text)
+def start_telebot():
+
+    def listener(messages):
+        """
+        When new messages arrive TeleBot will call this function.
+        """
+        for m in messages:
+            chatid = m.chat.id
+            if m.content_type == 'text':
+                if 'Kaixo' in m.text:
+                	text = 'Kaixo %s!' % (m.chat.firts_name)
+                    tb.send_message(chatid, text)
 
 
-tb = telebot.TeleBot(TOKEN)
-tb.set_update_listener(listener) #register listener
-tb.polling()
-#Use none_stop flag let polling will not stop when get new message occur error.
-tb.polling(none_stop=True)
-# Interval setup. Sleep 3 secs between request new message.
-#tb.polling(interval=3)
-while True: # Don't let the main Thread end.
-    pass
+    tb = telebot.TeleBot(TOKEN)
+    tb.set_update_listener(listener) #register listener
+    tb.polling()
+    #Use none_stop flag let polling will not stop when get new message occur error.
+    tb.polling(none_stop=True)
+    # Interval setup. Sleep 3 secs between request new message.
+    #tb.polling(interval=3)
+    while True: # Don't let the main Thread end.
+        pass
+
+class Command(BaseCommand):
+   help = 'Telebot'
+   def handle(self, *args, **options):
+       start_telebot()
