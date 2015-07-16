97f2f2aeb4f39c8266eae76188e13e44eea03f5b
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-07-16 17:33:35 +0200

    Telebot test

--- gamerauntsia/base/management/commands/telebot_script.py
+++ gamerauntsia/base/management/commands/telebot_script.py
@@ -1,25 +1,44 @@
 from django.core.management.base import BaseCommand
+from django_simple_forum.models import Category
 import telebot
 import time
+import re
 
 TOKEN = '107547414:AAEXaH2tSNcnaehNq_7NNbNKb1VfDbaa6Qs'
 
+COMMANDS = (
+    ('kaixo', 'Kaixo mundua!'),
+    ('foroa', 'Foroko aurkibidea ikusteko'),
+    ('laguntza', 'Laguntzeko prest naukazu!'),
+)
+
+
 def start_telebot():
-    def listener(messages):
-        """
-        When new messages arrive TeleBot will call this function.
-        """
-        for m in messages:
-            chatid = m.chat.id
-            if m.content_type == 'text':
-                if 'Kaixo' in m.text:
-                    print m
-                    text = 'Kaixo!'
-                    tb.send_message(chatid, text)
 
     tb = telebot.TeleBot(TOKEN)
-    tb.set_update_listener(listener) #register listener
-    tb.polling()
+
+    @tb.message_handler(func=lambda message: '#' in message.text)
+    def command_hashtag(message):
+        hashtags = ", ".join(re.findall(r"(?i)\#\w+", message.text))
+        tb.send_message(message.chat.id, "Fororako gai hauek aipatu dituzu: "+hashtags)
+
+    @tb.message_handler(commands=['kaixo', 'foroa', 'laguntza'])
+    def command_list(message):
+        if 'kaixo' in message.text:
+            tb.send_message(message.chat.id, "Kaixo %s!" % (message.from_user.first_name))
+        elif 'foroa' in message.text:
+            if not '-k' or '-f' or '-t' in message.text:
+                msg = 'Kategoriak:'
+                categories = "\n".join(f.title for f in Category.objects.all().order_by('title'))
+                msg += categories
+                
+            tb.send_message(message.chat.id, msg)
+        else:
+            cmd_lst = ''
+            for cmd in COMMANDS:
+                cmd_lst += "/%s - %s\n" % (cmd[0],cmd[1])
+            tb.send_message(message.chat.id, "Komando zerrenda:\n"+cmd_lst)
+
     #Use none_stop flag let polling will not stop when get new message occur error.
     tb.polling(none_stop=True)
     # Interval setup. Sleep 3 secs between request new message.
