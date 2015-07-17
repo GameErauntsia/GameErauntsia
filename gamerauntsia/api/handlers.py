a7d211937023fc9665c6c2a791c8ebfe3246387d
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-07-17 13:54:07 +0200

    Update handlers.py

--- gamerauntsia/api/handlers.py
+++ gamerauntsia/api/handlers.py
@@ -9,28 +9,28 @@ class MCHandler(BaseHandler):
    fields = ('mc_user','created','uuid','rol') 
 
    def read(self, request, username=None):
-        """
-        Returns a single post if `blogpost_id` is given,
-        otherwise a subset.
+       """
+       Returns a single post if `blogpost_id` is given,
+       otherwise a subset.
 
-        """
-        base = MC_Whitelist.objects
+       """
+       base = MC_Whitelist.objects
         
-        if username:
-        	try:
-	            mc = base.get(mc_user=username)
-	            data = {
-	                'user': mc.user.username,
-	                'mc_user': mc.mc_user,
-	        		'created': mc.created.strftime("%Y-%m-%d %H:%M:%S"),
-	                'rol': mc.get_rol_display(),
-	                'uuid': mc.uuid,
-	        	}
-	            return data
-	        except:
-	        	return False
-        else:
-            return False
+       if username:
+           try:
+	       mc = base.get(mc_user=username)
+	       data = {
+	           'user': mc.user.username,
+	           'mc_user': mc.mc_user,
+	           'created': mc.created.strftime("%Y-%m-%d %H:%M:%S"),
+	           'rol': mc.get_rol_display(),
+	           'uuid': mc.uuid,
+	       }
+	       return data
+	   except:
+	       return False
+       else:
+           return False
             
 class MCTelebotHandler(BaseHandler):
    allowed_methods = ('GET',)
@@ -39,16 +39,22 @@ class MCTelebotHandler(BaseHandler):
 
    def read(self, request, username=None):
         
-        base = MC_Whitelist.objects
+       base = MC_Whitelist.objects
         
-        if username:
-        	try:
-	            mc = base.get(mc_user=username)
-	            tb = telebot.TeleBot(settings.TELEBOT_TOKEN)
-	            msg = '[%s] %s-(r)en laguntza eskaera:\n%s' % (mc.get_rol_display(),username,request.GET.get('text', 'Mezurik gabe...'))
-	            tb.send_message(settings.MC_CHAT_ID, msg)
-	            return True
-	        except:
-	        	return False
-        else:
-            return False
+       if username:
+           try:
+	       mc = base.get(mc_user=username)
+	       tb = telebot.TeleBot(settings.TELEBOT_TOKEN)
+	       msg = '[%s] %s-(r)en laguntza eskaera:\n%s' % (mc.get_rol_display(),username,request.GET.get('text', 'Mezurik gabe...'))
+	       tb.send_message(settings.MC_CHAT_ID, msg)
+	       return True
+	   except:
+	       return False
+       else:
+           try:
+                tb = telebot.TeleBot(settings.TELEBOT_TOKEN)
+                msg = 'Anonimo baten laguntza eskaera:\n%s' % (request.GET.get('text', 'Mezurik gabe...'))
+                tb.send_message(settings.MC_CHAT_ID, msg)
+                return True
+            except:
+	        return False
