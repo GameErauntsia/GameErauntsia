cb231ab9395e2bf3f2b07db8a12fe2b74c422937
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-08-28 12:46:39 +0200

    Update forms.py

--- gamerauntsia/gameplaya/forms.py
+++ gamerauntsia/gameplaya/forms.py
@@ -6,6 +6,6 @@ from tinymce.widgets import TinyMCE
 class GamePlayAdminForm(forms.ModelForm):
     desk = forms.CharField(widget=TinyMCE(
                attrs={'cols': 80, 'rows': 50,},mce_attrs=settings.TINYMCE_BODY_CONFIG))
-    shared = forms.BooleanField(label="Sare sozialetan elkarbanatuta",readonly=True)
+    shared = forms.BooleanField(label="Sare sozialetan elkarbanatuta",widget = forms.CheckboxInput(attrs={'readonly':'readonly'}))
     class Meta:
         model = GamePlaya	
