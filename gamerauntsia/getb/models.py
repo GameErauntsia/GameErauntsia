bb5332599569696f7bc52457a22f426fb92c1d96
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-09-08 21:07:59 +0200

    GETB included

--- gamerauntsia/getb/models.py
+++ gamerauntsia/getb/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.conf import settings
 from photologue.models import Photo
-from gamerauntsia.jokoa.models import Jokoa, Plataforma
-from gamerauntsia.gamer.models import GamerUser
 from datetime import datetime
 from django.template import defaultfilters as filters
 from django.template.loader import get_template
@@ -13,31 +11,7 @@ STATUS = (
     ('1','Publikoa'),
 )
 
-class Kategoria(models.Model):
-    izena = models.CharField(max_length=64)
-    slug = models.SlugField(db_index=True, unique=True, help_text="Eremu honetan kategoria honen URL helbidea zehazten ari zara.")
-    desk = models.TextField(max_length=256,null=True,blank=True)
-    irudia = models.ForeignKey(Photo,null=True,blank=True)
-
-    class Meta:
-        verbose_name = "Kategoria"
-        verbose_name_plural = "Kategoriak"
-
-    def __unicode__(self):
-        return u'%s' % (self.izena)
-
-class Zailtasuna(models.Model):
-    izena = models.CharField(max_length=64)
-    slug = models.SlugField(db_index=True, unique=True, help_text="Eremu honetan zailtasun honen URL helbidea zehazten ari zara.")
-
-    class Meta:
-        verbose_name = "Zailtasuna"
-        verbose_name_plural = "Zailtasunak"
-
-    def __unicode__(self):
-        return u'%s' % (self.izena)
-
-class GamePlaya(models.Model):
+class Atala(models.Model):
     izenburua = models.CharField(max_length=64)
     slug = models.SlugField(unique=True,db_index=True, help_text="Eremu honetan game play honen URL helbidea zehazten ari zara.")
     desk = models.TextField(max_length=256)
@@ -47,16 +21,9 @@ class GamePlaya(models.Model):
     argazkia = models.ForeignKey(Photo)
     bideoa = models.CharField(max_length=100,null=True,blank=True,help_text="Eremu honetan Youtube bideoaren URL kodea itsatsi behar duzu. Adb.: c21XAuI3aMo")
 
-    jokoa = models.ForeignKey(Jokoa, related_name='gameplay')
-    plataforma = models.ForeignKey(Plataforma, related_name='gameplay')
-    zailtasuna = models.ForeignKey(Zailtasuna, related_name='gameplay')
-    kategoria = models.ManyToManyField(Kategoria, related_name='gameplay')
-
-    erabiltzailea = models.ForeignKey(GamerUser,related_name='gameplayak')
     publikoa_da = models.BooleanField(default=False, verbose_name="Publikatzeko prest")
     pub_date = models.DateTimeField('publikazio data', default=datetime.now)
     mod_date = models.DateTimeField('modifikazio data', default=datetime.now)
-    status = models.CharField(max_length=1, choices=STATUS, default='0')
     shared = models.BooleanField(default=False, help_text="Lauki hau automatikoki markatuko da sistemak edukia sare sozialetan elkarbanatzean.")
 
     def get_desk_index(self):
@@ -69,14 +36,6 @@ class GamePlaya(models.Model):
     	    return filters.striptags(self.desk)[:150]+'...'
         return filters.striptags(self.desk)
 
-    def get_puntuak(self):
-        if self.puntuak == 0:
-            return 0
-        return round(self.puntuak/self.botoak, .5)
-
-    def get_rating(self):
-        return int(self.get_puntuak()*2)
-
     def get_url(self):
         url = ''
         if self.bideoa.startswith('http://vimeo.com'):
@@ -86,13 +45,10 @@ class GamePlaya(models.Model):
     	return url
 
     def get_absolute_url(self):
-        return '%sgameplayak/%s' % (settings.HOST, self.slug)
+        return '%sgetb/%s' % (settings.HOST, self.slug)
 
     def getTwitText(self):
-        if self.erabiltzailea.twitter_id:
-            return self.izenburua + ' ' + self.get_absolute_url() + ' @%s 2dz' % (self.erabiltzailea.twitter_id)
-        else:
-            return self.izenburua + ' ' + self.get_absolute_url()
+        return self.izenburua + ' ' + self.get_absolute_url()
 
     def getEmailText(self):
        htmly = get_template('buletina/buletina.html')
@@ -118,11 +74,11 @@ class GamePlaya(models.Model):
             self.izenburua[0].upper()
         if not self.desk[0].isupper():
             self.desk[0].upper()
-        super(GamePlaya,self).save(*args,**kwargs)
+        super(Atala,self).save(*args,**kwargs)
 
     class Meta:
-        verbose_name = "Gameplaya"
-        verbose_name_plural = "Gameplayak"
+        verbose_name = "Atala"
+        verbose_name_plural = "Atalak"
 
     def __unicode__(self):
         return u'%s' % (self.izenburua)
---
+++ gamerauntsia/getb/views.py
@@ -0,0 +1,17 @@
+from gamerauntsia.gameplaya.models import Atala
+from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from datetime import datetime
+
+def index(request):
+    atalak = Atala.objects.filter(publikoa_da=True, pub_date__lt=datetime.now()).order_by('-pub_date')
+    return render_to_response('getb/index.html', locals(),context_instance=RequestContext(request))
+
+def atala(request,slug):
+    atala = get_object_or_404(Atala,slug=slug)
+    facebook_id = settings.FACEBOOK_APP_ID
+    return render_to_response('getb/atala.html', locals(),context_instance=RequestContext(request))
