b1b2bbdfc222cae6af24959249c46d779df62fe4
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-09-29 23:12:46 +0200

    Update urls.py

--- gamerauntsia/api/urls.py
+++ gamerauntsia/api/urls.py
@@ -8,6 +8,6 @@ auth = HttpBasicAuthentication(realm="Django Minecraft")
 mc_handler = Resource(MCHandler)
 mc_telebot_handler = Resource(MCTelebotHandler)
 urlpatterns = patterns('',
-    url(r'^get_minecraft_user/(?P<username>[-\w]+)/$', mc_handler),
+    url(r'^get_minecraft_user$', mc_handler),
     url(r'^send_mctelebot_msg/(?P<username>[-\w]+)/$', mc_telebot_handler),
 )
