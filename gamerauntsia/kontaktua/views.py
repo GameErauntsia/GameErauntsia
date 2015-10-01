fd0aa65d21e13e534adb6e00a2ac8d196fa1161a
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-10-01 16:17:23 +0200

    Update views.py

--- gamerauntsia/kontaktua/views.py
+++ gamerauntsia/kontaktua/views.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from gamerauntsia.berriak.models import Berria
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.mail import send_mail
@@ -7,7 +6,6 @@ from django.template import loader
 
 def index(request):
     h = {}
-    h['berriak'] = Berria.objects.all().order_by('-pub_date')[:5]
     return render_to_response('kontaktua/index.html', h,context_instance=RequestContext(request))
 
 def bidali(request):
@@ -17,5 +15,4 @@ def bidali(request):
     iruzkina = request.POST.get('iruzkina')
     tmpl = loader.render_to_string('kontaktua/email_tmpl.html',{'email':eposta,'body': iruzkina})
     send_mail(settings.EMAIL_SUBJECT+' '+gaia, tmpl, settings.DEFAULT_FROM_EMAIL,[settings.DEFAULT_TO_EMAIL], fail_silently=False)
-    h['berriak'] = Berria.objects.all().order_by('-pub_date')[:5]
     return render_to_response('kontaktua/bidalita.html', h,context_instance=RequestContext(request))
