d844267f757d54040c44e411da7e24f4b259b180
urtzai <uodriozola@codesyntax.com>
2015-10-27 17:01:40 +0100

    New paths

--- gamerauntsia/gamerauntsia/settings.py
+++ gamerauntsia/gamerauntsia/settings.py
@@ -56,7 +56,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/csmant/django/gamerauntsia/media/'
+MEDIA_ROOT = '/srv/data/web/vhosts/default/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -67,7 +67,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/csmant/django/gamerauntsia/static/'
+STATIC_ROOT = '/srv/data/web/vhosts/default/static/'
 
 STATIC_DOC_ROOT = STATIC_ROOT
 MEDIA_DOC_ROOT = MEDIA_ROOT
