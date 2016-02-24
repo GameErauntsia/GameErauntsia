25d56b2bbe3fc54eca748d51e6afb1e76ac837eb
ikerib <ikerib@gmail.com>
2016-02-24 10:24:15 +0100

    small fixes

--- gamerauntsia/gamerauntsia/gamer/urls.py
+++ gamerauntsia/gamerauntsia/gamer/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
-from django.conf import settings
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-	url(r'^$', 'gamerauntsia.gamer.views.index', name='gamer_index'),
-    url(r'^gamer_add_favorite_game/(?P<slug>[-\w]+)$', 'gamerauntsia.gamer.views.add_favorite_game', name='gamer_add_favorite_game'),
-    url(r'^(?P<username>[-\w]+)$', 'gamerauntsia.gamer.views.profile', name='gamer_profile'),
+                       url(r'^$', 'gamerauntsia.gamer.views.index', name='gamer_index'),
+                       url(r'^gamer_add_favorite_game/(?P<slug>[-\w]+)$', 'gamerauntsia.gamer.views.add_favorite_game',
+                           name='gamer_add_favorite_game'),
+                       url(r'^(?P<username>[-\w]+)$', 'gamerauntsia.gamer.views.profile', name='gamer_profile'),
 
-)
+                       )
