09bf808b455107bde69fdd907c290a7e130ed53c
ikerib <ikerib@gmail.com>
2016-02-24 10:29:00 +0100

    small fixes

--- gamerauntsia/gamerauntsia/berriak/forms.py
+++ gamerauntsia/gamerauntsia/berriak/forms.py
@@ -5,10 +5,13 @@ from tinymce.widgets import TinyMCE
 
 TINYMCE_BODY_CONFIG = getattr(settings, 'TINYMCE_BODY_CONFIG', {})
 
+
 class BerriaAdminForm(forms.ModelForm):
     desk = forms.CharField(widget=TinyMCE(
-               attrs={'cols': 80, 'rows': 50,},mce_attrs=TINYMCE_BODY_CONFIG))
-    shared = forms.BooleanField(label="Sare sozialetan elkarbanatuta",help_text="Lauki hau automatikoki markatuko da sistemak edukia sare sozialetan elkarbanatzean.", widget = forms.CheckboxInput(attrs={'readonly':'readonly'}),required=False)
+        attrs={'cols': 80, 'rows': 50,}, mce_attrs=TINYMCE_BODY_CONFIG))
+    shared = forms.BooleanField(label="Sare sozialetan elkarbanatuta",
+                                help_text="Lauki hau automatikoki markatuko da sistemak edukia sare sozialetan elkarbanatzean.",
+                                widget=forms.CheckboxInput(attrs={'readonly': 'readonly'}), required=False)
 
     class Meta:
         model = Berria
--- gamerauntsia/gamerauntsia/berriak/views.py
+++ gamerauntsia/gamerauntsia/berriak/views.py
@@ -1,18 +1,20 @@
-from gamerauntsia.berriak.models import Berria
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from datetime import datetime
+
+from django.conf import settings
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.conf import settings
-from datetime import datetime
-from django.shortcuts import get_object_or_404
+from gamerauntsia.berriak.models import Berria
+
 
 def index(request):
     h = {}
     zerr_berriak = Berria.objects.filter(status='1', pub_date__lt=datetime.now()).order_by('-pub_date')
     HOST = settings.HOST
-    return render_to_response('berriak/index.html', locals(),context_instance=RequestContext(request))
+    return render_to_response('berriak/index.html', locals(), context_instance=RequestContext(request))
 
-def berria(request,slug):
-    item = get_object_or_404(Berria,slug=slug)
+
+def berria(request, slug):
+    item = get_object_or_404(Berria, slug=slug)
     facebook_id = settings.FACEBOOK_APP_ID
-    return render_to_response('berriak/berria.html', locals(),context_instance=RequestContext(request))
+    return render_to_response('berriak/berria.html', locals(), context_instance=RequestContext(request))
