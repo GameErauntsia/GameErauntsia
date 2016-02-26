c88afa3c4e3f4438e2492b9e295b2d07c89b20d9
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2016-02-26 22:06:38 +0100

    Close #9

--- gamerauntsia/gamerauntsia/base/admin.py
+++ gamerauntsia/gamerauntsia/base/admin.py
@@ -10,3 +10,21 @@ class TerminoaAdmin(admin.ModelAdmin):
    
 
 admin.site.register(Terminoa, TerminoaAdmin)
+
+from photologue.admin import PhotoAdmin
+from photologue.models import Photo
+
+class PhotoAdmin2(PhotoAdmin):
+    def getphotoadmin(self, obj):
+        """ """
+        try:
+            return u'<img src="%s" />' % obj.get_admin_thumbnail_url()
+        except:
+            return '%s' % (obj.title)
+    getphotoadmin.allow_tags = True
+    getphotoadmin.short_description = 'Irudia'
+
+    list_display = ('getphotoadmin', 'title', 'date_added', 'is_public')
+
+admin.site.unregister(Photo)
+admin.site.register(Photo, PhotoAdmin2)
