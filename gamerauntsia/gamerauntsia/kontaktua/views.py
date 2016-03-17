b35ac81efaabe373f5ccb296cf98648c9ed16f4b
ikerib <ikerib@gmail.com>
2016-03-17 23:05:05 +0100

    Small fix

--- gamerauntsia/gamerauntsia/kontaktua/views.py
+++ gamerauntsia/gamerauntsia/kontaktua/views.py
@@ -7,10 +7,12 @@ from django.core.mail import send_mail
 from django.template import loader
 from .forms import ContactForm
 
+
 def index(request):
     h = {}
     form = ContactForm()
-    return render_to_response('kontaktua/index.html', locals(),context_instance=RequestContext(request))
+    return render_to_response('kontaktua/index.html', locals(), context_instance=RequestContext(request))
+
 
 def bidali(request):
     h = {}
@@ -20,8 +22,9 @@ def bidali(request):
             eposta = request.POST.get('eposta')
             gaia = request.POST.get('gaia')
             iruzkina = request.POST.get('iruzkina')
-            tmpl = loader.render_to_string('kontaktua/email_tmpl.html',{'email':eposta,'body': iruzkina})
-            send_mail(settings.EMAIL_SUBJECT+' '+gaia, tmpl, settings.DEFAULT_FROM_EMAIL,[settings.DEFAULT_TO_EMAIL], fail_silently=False)
-            return render_to_response('kontaktua/bidalita.html', h,context_instance=RequestContext(request))
+            tmpl = loader.render_to_string('kontaktua/email_tmpl.html', {'email': eposta, 'body': iruzkina})
+            send_mail(settings.EMAIL_SUBJECT + ' ' + gaia, tmpl, settings.DEFAULT_FROM_EMAIL,
+                      [settings.DEFAULT_TO_EMAIL], fail_silently=False)
+            return render_to_response('kontaktua/bidalita.html', h, context_instance=RequestContext(request))
         return HttpResponseRedirect(reverse('kontaktua'))
     return render_to_response('kontaktua/index.html', locals(), context_instance=RequestContext(request))
