2438c2b553fc8206ed43109fecef2b7b0759e33d
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-05-11 17:29:23 +0200

    Close #36

--- gamerauntsia/gamerauntsia/base/management/commands/calculate_karma.py
+++ gamerauntsia/gamerauntsia/base/management/commands/calculate_karma.py
@@ -1,14 +1,18 @@
 from django.core.management.base import BaseCommand
 from gamerauntsia.gamer.models import GamerUser
  
-def calc_karma():
+def calc_karma(days):
     users = GamerUser.objects.filter(is_active=True)
 
     for user in users:
-        user.karma = user.get_karma()
+        user.karma = user.get_karma(days)
         user.save()
 
 class Command(BaseCommand):
     help = 'Calculate users karma'
+
+    def add_arguments(self, parser):
+        parser.add_argument('days', type=int)
+
     def handle(self, *args, **options):
-        calc_karma()
+        calc_karma(days=options.get('days', 30))
