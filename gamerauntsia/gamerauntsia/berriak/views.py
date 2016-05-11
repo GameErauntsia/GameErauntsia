de4a770ec3b6187c756b835f4473c9b2affd3323
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-05-11 16:30:16 +0200

    Fix #33

--- gamerauntsia/gamerauntsia/berriak/views.py
+++ gamerauntsia/gamerauntsia/berriak/views.py
@@ -4,13 +4,12 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from gamerauntsia.berriak.models import Berria
+from gamerauntsia.berriak.models import Berria, Gaia
 
 
 def index(request):
     h = {}
     zerr_berriak = Berria.objects.filter(status='1', pub_date__lt=datetime.now()).order_by('-pub_date')
-    HOST = settings.HOST
     return render_to_response('berriak/index.html', locals(), context_instance=RequestContext(request))
 
 
@@ -18,3 +17,8 @@ def berria(request, slug):
     item = get_object_or_404(Berria, slug=slug)
     facebook_id = settings.FACEBOOK_APP_ID
     return render_to_response('berriak/berria.html', locals(), context_instance=RequestContext(request))
+
+def gaia(request,slug):
+    gaia = get_object_or_404(Gaia, slug=slug)
+    zerr_berriak = Berria.objects.filter(gaia=gaia,status='1', pub_date__lt=datetime.now()).order_by('-pub_date')
+    return render_to_response('berriak/gaia.html', locals(), context_instance=RequestContext(request))
--- gamerauntsia/gamerauntsia/jokoen_itzulpenak/admin.py
+++ gamerauntsia/gamerauntsia/jokoen_itzulpenak/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 from models import Itzulpena 
-admin.site.register(Itzulpena)
+
+class ItzulpenakAdmin(admin.ModelAdmin):
+
+    def admin_thumbnail(self,obj):
+        try:
+            if obj.jokoa and obj.jokoa.logoa:
+                return u'<img src="%s" />' % (obj.jokoa.logoa.get_admin_thumbnail_url())
+            else:
+                return u'(Irudirik ez)'
+        except:
+            return '%s' % (obj.jokoa.logoa.title)
+    admin_thumbnail.short_description = 'Thumb'
+    admin_thumbnail.allow_tags = True
+
+    list_display = ('admin_thumbnail', 'izena', 'jokoa', 'plataforma', 'pub_date', 'mod_date','status','publikoa_da')
+    list_display_links = ('izena',)
+    list_filter = ('publikoa_da', 'status')
+    search_fields = ['izena','jokoa__izena']
+    ordering = ('-pub_date',)
+
+admin.site.register(Itzulpena, ItzulpenakAdmin)
