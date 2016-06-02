c65ec480a5b6b132af1282d6c415e4bb7e7eb00d
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-06-02 09:22:23 +0200

    Update views.py

--- gamerauntsia/gamerauntsia/api/views.py
+++ gamerauntsia/gamerauntsia/api/views.py
@@ -2,13 +2,14 @@ import telebot
 from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, renderer_classes, permission_classes
 from gamerauntsia.zerbitzariak.models import MC_Whitelist
 
 
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
-@permission_classes((permissions.AllowAny,))
+@permission_classes((AllowAny,))
 def mc_whitelist(request, format=None):
     
     base = MC_Whitelist.objects
@@ -48,7 +49,7 @@ def mc_whitelist(request, format=None):
 
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
-@permission_classes((permissions.AllowAny,))
+@permission_classes((AllowAny,))
 def mc_telebot(request, username, format=None):                  
     base = MC_Whitelist.objects
     
