29c2ac6dabf704ad08d0105bc546c20dcf1ab3ab
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-11-14 17:05:09 +0100

    Update urls.py

--- gamerauntsia/gamerauntsia/utils/urls.py
+++ gamerauntsia/gamerauntsia/utils/urls.py
@@ -6,7 +6,7 @@ def get_urljson(url):
     stream = []
     req = urllib2.Request(url)
     opener = urllib2.build_opener()
-    f = opener.open(req)
+    f = opener.open(req, ,timeout=1)
     try:
         stream = json.load(f)
     except:
@@ -15,7 +15,7 @@ def get_urljson(url):
 
 def get_urlxml(url):
     stream = []
-    raw_data = urllib2.urlopen(url)
+    raw_data = urllib2.urlopen(url, timeout=1)
     data = raw_data.read()
     raw_data.close()
     stream = dict(xmltodict.parse(data))
