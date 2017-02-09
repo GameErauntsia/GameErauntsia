c700bb97731cf437383dab9eec906e34d7c7c6ca
Game Erauntsia <ge@gamerauntsia.eus>
2017-02-09 14:34:24 +0000

    New wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
-import sys
-import os
-import os.path
-from django.core.wsgi import get_wsgi_application
+"""
+WSGI config for tribualserver project.
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                    'gamerauntsia')))
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamerauntsia.settings")
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
