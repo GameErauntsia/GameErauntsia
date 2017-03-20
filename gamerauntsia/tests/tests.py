e4002f425bace7830ead4333a4aac3b341fa14af
Urtzi Odriozola <uodriozola@codesyntax.com>
2017-03-20 16:41:55 +0100

    test bugfix

--- gamerauntsia/tests/tests.py
+++ gamerauntsia/tests/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class BasicTest(TestCase):
     def setUp(self):
         user = User.objects.create_user('urtzai', 'uodriozola@gmail.com', 'urtzaipass')
-
 
     def test_index(self):
         c = Client()
