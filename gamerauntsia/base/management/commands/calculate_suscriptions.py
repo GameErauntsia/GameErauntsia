3034fb74da838bfb4975f1b49bd4fe91723c1df3
Urtzi Odriozola <uodriozola@codesyntax.com>
2017-12-04 16:18:04 +0100

    Create calculate_suscriptions.py

---
+++ gamerauntsia/base/management/commands/calculate_suscriptions.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from datetime import datetime
+from gamerauntsia.bazkidetza.models import Bazkidea
+ 
+def calc_suscription():
+    users = Bazkidea.objects.filter(is_paid=True, expire_date__gt=datetime.now())
+
+    for user in users:
+        user.paid = False
+        user.save()
+
+class Command(BaseCommand):
+    help = 'Calculate users suscription'
+
+    def handle(self, *args, **options): 
+        calc_suscription()
