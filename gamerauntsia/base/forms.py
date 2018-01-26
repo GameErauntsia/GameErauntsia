97abe69bf6e1d9a597edb16283f533d13e1ed3be
Urtzi Odriozola <uodriozola@codesyntax.com>
2018-01-26 18:01:53 +0100

    test error fixed

--- gamerauntsia/base/forms.py
+++ gamerauntsia/base/forms.py
@@ -3,9 +3,11 @@ from django.conf import settings
 from django.contrib.flatpages.models import FlatPage
 from tinymce.widgets import TinyMCE
 
-class FlatPageForm(forms.ModelForm):
+TINYMCE_DEFAULT_CONFIG = getattr(settings, 'TINYMCE_DEFAULT_CONFIG', {})
 
-    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}, mce_attrs=settings.TINYMCE_DEFAULT_CONFIG))
+
+class FlatPageForm(forms.ModelForm):
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}, mce_attrs=TINYMCE_DEFAULT_CONFIG))
 
     class Meta:
         model = FlatPage
