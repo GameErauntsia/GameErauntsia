1613dfd9ed7c3933b1240a399d33db10303efec6
Urtzi Odriozola <urtzi.odriozola@gmail.com>
2019-07-02 23:44:35 +0200

    Update admin.py

--- gamerauntsia/gameplaya/admin.py
+++ gamerauntsia/gameplaya/admin.py
@@ -2,10 +2,12 @@ from gamerauntsia.gameplaya.models import GamePlaya, Kategoria, Zailtasuna
 from django.contrib import admin
 from django.conf import settings
 from gamerauntsia.gameplaya.forms import GamePlayAdminForm
+from django.utils.safestring import mark_safe
 
 
 class GamePlayAdmin(admin.ModelAdmin):
 
+    @mark_safe
     def admin_thumbnail(self,obj):
         try:
             if obj.argazkia:
