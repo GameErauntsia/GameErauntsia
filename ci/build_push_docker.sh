47d7eeb879c16c03e2726e827c3234546e2a0f3f
Bingen Galartza Iparragirre <bingen@galaipa.eus>
2021-02-09 20:32:17 +0100

    Build Docker image in Travis pipelines
    The goal si to replace the DockerHub hook

---
+++ ci/build_push_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+TARGET_IMAGE="${REGISTRY_URL}/${IMAGE_NAME}"
+
+docker build -t $TARGET_IMAGE .
+
+docker login --username $REGISTRY_USERNAME \
+             --password $REGISTRY_PASSWORD \
+             $REGISTRY_ADDRESS
+
+# Latest
+TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
+docker tag ${TARGET_IMAGE} ${TARGET_IMAGE_LATEST}
+docker push ${TARGET_IMAGE_LATEST}
+
+# Versioned
+TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
+TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
+docker tag ${TARGET_IMAGE} ${TARGET_IMAGE_VERSIONED}
+docker push ${TARGET_IMAGE_VERSIONED}
