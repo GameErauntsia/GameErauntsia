7089f6d7d0a4feb4e67ac52cc8c3c381012255e8
Bingen Galartza Iparragirre <bingen@galaipa.eus>
2021-07-25 13:18:48 +0200

    Use previous Docker image for building the new one
    The goal is to make the pipeline faster

--- ci/build_push_docker.sh
+++ ci/build_push_docker.sh
@@ -1,20 +1,21 @@
 #!/bin/bash -e
 
 TARGET_IMAGE="${REGISTRY_URL}/${IMAGE_NAME}"
+TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
+VERSION=$(date '+%Y-%m-%d__%H-%M-%S')
+TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
 
-docker build -t $TARGET_IMAGE .
+docker pull $TARGET_IMAGE_LATEST
+docker build -t $TARGET_IMAGE --cache-from $TARGET_IMAGE_LATEST .
 
 docker login --username $REGISTRY_USERNAME \
              --password $REGISTRY_PASSWORD \
              $REGISTRY_ADDRESS
 
 # Latest
-TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 docker tag ${TARGET_IMAGE} ${TARGET_IMAGE_LATEST}
 docker push ${TARGET_IMAGE_LATEST}
 
 # Versioned
-VERSION=$(date '+%Y-%m-%d__%H-%M-%S')
-TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
 docker tag ${TARGET_IMAGE} ${TARGET_IMAGE_VERSIONED}
 docker push ${TARGET_IMAGE_VERSIONED}
