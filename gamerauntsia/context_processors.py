df1a4e53e809833ca356de32222ac846d8d9184c
Bingen Galartza Iparragirre <bingen@galaipa.eus>
2021-12-18 23:08:46 +0100

    Remove printed commited by accident

--- gamerauntsia/context_processors.py
+++ gamerauntsia/context_processors.py
@@ -4,5 +4,4 @@ def fb_app_id(request):
     return {'FB_APP_ID': settings.FB_APP_ID}
 
 def enable_tracking(request):
-    print(settings.ENABLE_TRACKING)
     return {'ENABLE_TRACKING': settings.ENABLE_TRACKING}
