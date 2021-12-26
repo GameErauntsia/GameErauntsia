e6e4d6f94f5910d6b28153a69d6eb4c05dc6a52f
Bingen Galartza Iparragirre <bingen@galaipa.eus>
2021-12-26 14:27:09 +0100

    Add MC server status (version,and player list) to the server page sidebar

--- gamerauntsia/zerbitzariak/views.py
+++ gamerauntsia/zerbitzariak/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from gamerauntsia.zerbitzariak.models import MC_Whitelist
 from gamerauntsia.gamer.models import JokuPlataforma
 from gamerauntsia.utils.urls import get_urljson
+from mcstatus import MinecraftServer
 from .forms import MCForm
+import re
 
 
 def set_user_whitelist(user, nick, action=None):
@@ -31,16 +33,30 @@ def set_user_whitelist(user, nick, action=None):
             return True
     return False
 
-
 def minecraft_server(request):
     user = request.user
     mc_list = MC_Whitelist.objects.all().order_by('-created')[:10]
     mc_admin_list = MC_Whitelist.objects.filter(rol='a')
-    mc_vip_list = MC_Whitelist.objects.filter(rol='v')
     mc_form = MCForm()
     carousel_images = range(1,21)
     return render(request, 'zerbitzariak/minecraft.html', locals())
 
+def get_mc_version_string(mc_server_status):
+    try:
+        long_version = mc_server_status.version.name
+        short_version = re.findall('[0-9]+\.[0-9]+\.[0-9]+',long_version)[0]
+        return short_version
+    except:
+        return None
+
+def minecraft_status(request):
+    try:
+        mc_server = MinecraftServer.lookup("mc.gamerauntsia.eus")
+        mc_server_status = mc_server.status()
+        mc_server_version = get_mc_version_string(mc_server_status)
+    except:
+        mc_server = None
+    return render(request, 'zerbitzariak/minecraft_status.html', locals())
 
 def minecraft_mapa(request):
     user = request.user
