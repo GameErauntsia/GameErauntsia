bcca485495da561a1e0f91e2a38ed73ca647eeb6
Bingen Galartza Iparragirre <bingen@galaipa.eus>
2022-01-09 12:48:02 +0100

    Fix contact form email sending and add logging

--- gamerauntsia/kontaktua/views.py
+++ gamerauntsia/kontaktua/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django.template import loader
 from .forms import ContactForm
+
+import logging
+logger = logging.getLogger(__name__)
 
 def contact_form(request):
     if request.method == 'GET':
@@ -17,14 +20,17 @@ def contact_form(request):
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
             try:
-                email_content = loader.render_to_string('kontaktua/email_tmpl.html', {'email': from_email, 'body': message})
-                send_mail(settings.EMAIL_SUBJECT + ' ' + subject,
-                          email_content,
-                          settings.DEFAULT_FROM_EMAIL,
-                          [settings.DEFAULT_TO_EMAIL],
-                          reply_to=[from_email],
-                          fail_silently=False)
+                email_body = loader.render_to_string('kontaktua/email_tmpl.html', {'email': from_email, 'body': message})
+                email = EmailMessage(settings.EMAIL_SUBJECT + ' ' + subject,
+                                     email_body,
+                                     from_email=settings.DEFAULT_FROM_EMAIL,
+                                     to=[settings.DEFAULT_TO_EMAIL],
+                                     reply_to=[from_email])
+                result = email.send(fail_silently=False)
+                if result == 1:
+                    return render(request, 'kontaktua/bidalita.html', {})
+                else:
+                    logger.error("Could not send contact email")
             except:
-                return render(request, 'kontaktua/index.html', {'form': form})
-            return render(request, 'kontaktua/bidalita.html', {})
+                logger.exception("Could not send contact email")
     return render(request, 'kontaktua/index.html', {'form': form})
