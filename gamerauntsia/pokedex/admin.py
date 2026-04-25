6631bdb4b425c9cec04d816a16876849ddae13ff
Bingen Galartza Iparragirre <bingen@galaipa.eus>
2026-04-25 10:25:02 +0200

    Pokedex entitate berriak sartu
    [Re #233]

--- gamerauntsia/pokedex/admin.py
+++ gamerauntsia/pokedex/admin.py
@@ -1,4 +1,16 @@
-from .models import Pokemon, PokemonGeneration
+from .models import (
+    Pokemon,
+    PokemonGeneration,
+    PokemonItem,
+    PokemonCharacter,
+    PokemonPokedex,
+    PokemonType,
+    PokemonAbility,
+    PokemonMove,
+    PokemonRegion,
+    PokemonLocation,
+    PokemonNature,
+)
 from django.contrib import admin
 
 
@@ -8,6 +20,47 @@ class PokemonGenerationAdmin(admin.ModelAdmin):
     list_display = [
         "id",
     ]
+
+
+class PokemonResourceAdmin(admin.ModelAdmin):
+    ordering = ["id"]
+    list_display = ["id", "izena_ingelesez", "izena_euskaraz"]
+
+
+class PokemonItemAdmin(PokemonResourceAdmin):
+    model = PokemonItem
+
+
+class PokemonCharacterAdmin(PokemonResourceAdmin):
+    model = PokemonCharacter
+
+
+class PokemonTypeAdmin(PokemonResourceAdmin):
+    model = PokemonType
+
+
+class PokemonPokedexAdmin(PokemonResourceAdmin):
+    model = PokemonPokedex
+
+
+class PokemonAbilityAdmin(PokemonResourceAdmin):
+    model = PokemonAbility
+
+
+class PokemonMoveAdmin(PokemonResourceAdmin):
+    model = PokemonMove
+
+
+class PokemonRegionAdmin(PokemonResourceAdmin):
+    model = PokemonRegion
+
+
+class PokemonLocationAdmin(PokemonResourceAdmin):
+    model = PokemonLocation
+
+
+class PokemonNatureAdmin(PokemonResourceAdmin):
+    model = PokemonNature
 
 
 class PokemonAdmin(admin.ModelAdmin):
@@ -24,4 +77,13 @@ class PokemonAdmin(admin.ModelAdmin):
 
 
 admin.site.register(PokemonGeneration, PokemonGenerationAdmin)
+admin.site.register(PokemonItem, PokemonItemAdmin)
+admin.site.register(PokemonCharacter, PokemonCharacterAdmin)
+admin.site.register(PokemonType, PokemonTypeAdmin)
+admin.site.register(PokemonPokedex, PokemonPokedexAdmin)
+admin.site.register(PokemonAbility, PokemonAbilityAdmin)
+admin.site.register(PokemonMove, PokemonMoveAdmin)
+admin.site.register(PokemonRegion, PokemonRegionAdmin)
+admin.site.register(PokemonLocation, PokemonLocationAdmin)
+admin.site.register(PokemonNature, PokemonNatureAdmin)
 admin.site.register(Pokemon, PokemonAdmin)
--- gamerauntsia/pokedex/models.py
+++ gamerauntsia/pokedex/models.py
@@ -13,6 +13,94 @@ class PokemonGeneration(models.Model):
         verbose_name_plural = "Belaunaldiak"
 
 
+class PokemonResource(models.Model):
+    id = models.IntegerField(primary_key=True)
+    izena_ingelesez = models.CharField(max_length=256, verbose_name="Izena ingelesez")
+    izena_euskaraz = models.CharField(max_length=256, verbose_name="Izena euskaraz")
+    deskribapena = models.TextField(max_length=256, null=True, blank=True)
+
+
+class PokemonItem(PokemonResource):
+    def __str__(self):
+        return "%d.objektua" % (self.id)
+
+    class Meta:
+        verbose_name = "Objektu"
+        verbose_name_plural = "Objektuak"
+
+
+class PokemonCharacter(PokemonResource):
+    def __str__(self):
+        return "%d.pertsonaia" % (self.id)
+
+    class Meta:
+        verbose_name = "Pertsonaia"
+        verbose_name_plural = "Pertsonaiak"
+
+
+class PokemonPokedex(PokemonResource):
+    def __str__(self):
+        return "%d.pokedexa" % (self.id)
+
+    class Meta:
+        verbose_name = "Pokedexa"
+        verbose_name_plural = "Pokedexak"
+
+
+class PokemonType(PokemonResource):
+    def __str__(self):
+        return "%d.mota" % (self.id)
+
+    class Meta:
+        verbose_name = "Mota"
+        verbose_name_plural = "Motak"
+
+
+class PokemonAbility(PokemonResource):
+    def __str__(self):
+        return "%d.gaitasuna" % (self.id)
+
+    class Meta:
+        verbose_name = "Gaitasuna"
+        verbose_name_plural = "Gaitasunak"
+
+
+class PokemonMove(PokemonResource):
+    def __str__(self):
+        return "%d.erasoa" % (self.id)
+
+    class Meta:
+        verbose_name = "Erasoa"
+        verbose_name_plural = "Erasoak"
+
+
+class PokemonRegion(PokemonResource):
+    def __str__(self):
+        return "%d.lurraldea" % (self.id)
+
+    class Meta:
+        verbose_name = "Lurraldea"
+        verbose_name_plural = "Lurradeak"
+
+
+class PokemonLocation(PokemonResource):
+    def __str__(self):
+        return "%d.tokia" % (self.id)
+
+    class Meta:
+        verbose_name = "Tokia"
+        verbose_name_plural = "Tokiak"
+
+
+class PokemonNature(PokemonResource):
+    def __str__(self):
+        return "%d.izaera" % (self.id)
+
+    class Meta:
+        verbose_name = "Izaera"
+        verbose_name_plural = "Izaerak"
+
+
 class Pokemon(models.Model):
     id = models.IntegerField(primary_key=True)
     pokemon_generation = models.ForeignKey(
